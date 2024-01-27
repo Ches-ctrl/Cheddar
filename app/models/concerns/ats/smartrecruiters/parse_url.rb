5da13c91dfe93e67218c5c9c16cd84a185705a4a
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-01-27 22:26:58 +0000

    working, to add api links and job details next

--- app/models/concerns/ats/smartrecruiters/parse_url.rb
+++ app/models/concerns/ats/smartrecruiters/parse_url.rb
@@ -3,12 +3,12 @@ module Ats::Smartrecruiters::ParseUrl
 
   def self.parse_url(url)
     regex_formats = [
-      %r{https://jobs\.smartrecruiters\.com/(?<ats_identifier>[^/]+)/(?<job_id>\d+)-[^/]+},
+      %r{https://jobs\.smartrecruiters\.com/(?<ats_identifier>[^/]+)/(?<job_id>\d+)(?:-[^/]+)?},
+      # TODO: Add API parsing for Smartrecruiters
     ]
 
     regex_formats.each do |regex|
       match = url.match(regex)
-      p match
       if match
         ats_identifier, job_id = match.captures
         return [ats_identifier, job_id]
