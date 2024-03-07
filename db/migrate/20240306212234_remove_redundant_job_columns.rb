dbc2ed83d6dba018108465edb44e1a6041fcdf38
Daniel Sussman <daniel.o.sussman@gmail.com>
2024-03-07 08:57:29 +0000

    Update and rename 20240306212234_remove_redundant_job_coolumns.rb to 20240306212234_remove_redundant_job_columns.rb
    jobs.location is retained but the name is changed to avoid ambiguity

--- db/migrate/20240306212234_remove_redundant_job_columns.rb
+++ db/migrate/20240306212234_remove_redundant_job_columns.rb
@@ -1,6 +1,6 @@
-class RemoveRedundantJobCoolumns < ActiveRecord::Migration[7.1]
+class RemoveRedundantJobColumns < ActiveRecord::Migration[7.1]
   def change
-    remove_column :jobs, :location
+    rename_column :jobs, :location, :non_geocoded_location_string
     remove_column :jobs, :city
     remove_column :jobs, :latitude
     remove_column :jobs, :longitude
