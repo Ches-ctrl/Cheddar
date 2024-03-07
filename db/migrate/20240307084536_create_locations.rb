6d274fdea79e13bd32d6a271d5b70a298005fc73
Daniel Sussman <daniel.o.sussman@gmail.com>
2024-03-07 08:46:25 +0000

    Update and rename 20240306193936_create_locations.rb to 20240307084536_create_locations.rb
    Changed bigint to references

--- db/migrate/20240307084536_create_locations.rb
+++ db/migrate/20240307084536_create_locations.rb
@@ -2,7 +2,7 @@ class CreateLocations < ActiveRecord::Migration[7.1]
   def change
     create_table :locations do |t|
       t.string :city
-      t.bigint :country_id
+      t.references :country, null: false, foreign_key: true
       t.decimal :latitude, precision: 10, scale: 6
       t.decimal :longitude, precision: 10, scale: 6
 
