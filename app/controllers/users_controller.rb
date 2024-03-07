ee6b519143b1a5113c93ed7e58fcb7267082cc6a
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-03-07 23:08:30 +0000

    tidying up profile page ready to fix tomorrow and fixing n+1 query issue on the page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ require 'yomu'
 class UsersController < ApplicationController
   def show
     @user = current_user
-    @job_applications = JobApplication.where(user_id: current_user)
+    @job_applications = JobApplication.includes(job: [:company]).where(user_id: current_user)
   end
 
   def fetch_template
