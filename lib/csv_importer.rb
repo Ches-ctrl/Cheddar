212c50379ec1469fdea611bc5181e41a32767663
Steven R. Baker <steven@stevenrbaker.com>
2024-04-04 17:37:31 +0200

    Clean up the bunk data handling.

--- lib/csv_importer.rb
+++ lib/csv_importer.rb
@@ -10,24 +10,9 @@ class CsvImporter
       company = Company.find_or_create_by company_name: row["Company"]
 
       country = Country.find_or_create_by name: "United Kingdom"
-      if row["Location"] == "United Kingdom"
-        locations = [Location.find_or_create_by(city: "Any", country: country)]
-      elsif row["Location"].include? '|'
-        city_names = row["Location"].split('|').map(&:strip)
+      locations = create_locations(country, row)
 
-        locations = city_names.map do |city_name|
-          Location.find_or_create_by city: city_name, country: country
-        end
-      elsif row["Location"].include? '-'
-        city_name = row["Location"].split('-').last.strip
-
-        locations = [Location.find_or_create_by(city: city_name, country: country)]
-      else
-        locations = [Location.find_or_create_by(city: row["Location"], country: country)]
-      end
-
-      deadline = nil if row["Deadline"] == "Rolling deadline"
-      deadline = Date.parse row["Deadline"] unless row["Deadline"] == "Rolling deadline"
+      deadline = date_for(row)
 
       Job.create industry: row["Sector"],
                  job_title: row["Job Title"],
@@ -37,6 +22,30 @@ class CsvImporter
                  locations: locations,
                  job_description: row["Short Description"],
                  seniority: row["Job-Type"]
+    end
+  end
+
+  private
+
+  def date_for row
+    Date.parse row["Deadline"] unless row["Deadline"] == "Rolling deadline"
+  end
+
+  def create_locations country, row
+    if row["Location"] == "United Kingdom"
+      [Location.find_or_create_by(city: "Any", country: country)]
+    elsif row["Location"].include? '|'
+      city_names = row["Location"].split('|').map(&:strip)
+
+      city_names.map do |city_name|
+        Location.find_or_create_by city: city_name, country: country
+      end
+    elsif row["Location"].include? '-'
+      city_name = row["Location"].split('-').last.strip
+
+      [Location.find_or_create_by(city: city_name, country: country)]
+    else
+      [Location.find_or_create_by(city: row["Location"], country: country)]
     end
   end
 end
