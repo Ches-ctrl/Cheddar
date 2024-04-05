4ed47e87331eac464c0a654b224aa72051b6a026
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-04-05 19:21:56 +0100

    we can just get the application form from the api...

---
+++ app/services/get_form.rb
@@ -0,0 +1,22 @@
+require 'net/http'
+require 'json'
+
+class GetForm
+
+  def self.perform
+    url = URI("https://boards-api.greenhouse.io/v1/boards/codepath/jobs/4035993007?questions=true&pay_transparency=true")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    request = Net::HTTP::Get.new(url)
+    response = http.request(request)
+
+    if response.code == '200'
+      data = JSON.parse(response.body)
+      questions = data['questions']
+      p questions
+    else
+      puts "Failed to retrieve the form. Error: #{response.code} - #{response.message}"
+    end
+  end
+end
