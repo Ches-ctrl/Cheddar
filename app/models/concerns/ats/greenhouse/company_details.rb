4d1ca1e8cf277a2422669a5a9bbca8c1b41220cf
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-04-05 19:27:51 +0100

    removing commented code

--- app/models/concerns/ats/greenhouse/company_details.rb
+++ app/models/concerns/ats/greenhouse/company_details.rb
@@ -24,33 +24,6 @@ module Ats
         data = JSON.parse(response)
         [data['name'], data['content']]
       end
-
-      # def check_for_details(company, ats_system, ats_identifier, description)
-      #   if company.description.nil?
-      #     p "Missing description for #{company.company_name}"
-      #     company.update(description:)
-      #   end
-
-      #   if company.ats_identifier.nil?
-      #     p "Missing ATS identifier for #{company.company_name}"
-      #     company.update(ats_identifier:)
-      #   end
-
-      #   if company.applicant_tracking_system_id.nil?
-      #     p "Missing ATS system for #{company.company_name}"
-      #     company.update(applicant_tracking_system_id: ats_system.id)
-      #   end
-
-      #   if company.url_ats_api.nil?
-      #     p "Missing ATS API URL for #{company.company_name}"
-      #     company.update(url_ats_api: "#{ats_system.base_url_api}#{ats_identifier}")
-      #   end
-
-      #   return unless company.url_ats_main.nil?
-
-      #   p "Missing ATS Main URL for #{company.company_name}"
-      #   company.update(url_ats_main: "#{ats_system.base_url_main}#{ats_identifier}")
-      # end
     end
   end
 end
