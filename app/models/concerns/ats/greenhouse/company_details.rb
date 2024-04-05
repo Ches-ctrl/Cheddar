66a18ebbf7db320c824471355cc4f2070e16b2b4
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-04-05 20:36:00 +0100

    check for redirect to greenhouse

--- app/models/concerns/ats/greenhouse/company_details.rb
+++ app/models/concerns/ats/greenhouse/company_details.rb
@@ -24,6 +24,36 @@ module Ats
         data = JSON.parse(response)
         [data['name'], data['content']]
       end
+
+      def check_for_careers_url_redirect(company)
+        url = URI(company.url_ats_main)
+        http = Net::HTTP.new(url.host, url.port)
+        http.use_ssl = true if url.scheme == 'https'
+        request = Net::HTTP::Get.new(url.request_uri)
+
+        max_retries = 2
+        retries = 0
+        begin
+          response = http.request(request)
+        rescue Errno::ECONNRESET, OpenSSL::SSL::SSLError => e
+          retries += 1
+          if retries <= max_retries
+            sleep(2**retries) # Exponential backoff
+            retry
+          else
+            puts "Check for careers redirect failed after #{max_retries} retries: #{e.message}"
+            return false
+          end
+        end
+
+        if response.is_a?(Net::HTTPRedirection)
+          redirected_url = URI.parse(response['Location'])
+          company.update(url_careers: redirected_url)
+          company.update(company_website_url: redirected_url.host)
+        else
+          p "No redirect for #{company.url_ats_main}"
+        end
+      end
     end
   end
 end
