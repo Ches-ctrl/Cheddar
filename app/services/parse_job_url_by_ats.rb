b3d42d73fb4f9d5ea4a4877aade039215c74413a
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-04-06 15:25:13 +0100

    delete original code

--- app/services/parse_job_url_by_ats.rb
+++ app/services/parse_job_url_by_ats.rb
@@ -9,14 +9,9 @@ class ParseJobUrlByAts
   end
 
   def parse(list = nil)
-    return unless (ats = fetch_ats)
+    return unless (ats = determine_ats)
 
     ats_identifier, job_id = list ? ats.parse_url(@string, list[ats.name]) : ats.parse_url(@string)
     [ats, ats_identifier, job_id]
-  end
-
-  def fetch_ats
-    name = ATS_SYSTEM_PARSER.find { |regex, ats_name| break ats_name if @string.match?(regex) }
-    return ApplicantTrackingSystem.find_by(name:)
   end
 end
