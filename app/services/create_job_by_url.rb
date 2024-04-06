eb9cd86d65c31da261ca5e7d9c74945b6c6c9d67
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-04-06 16:56:32 +0100

    only find or create companies for supported ats systems, otherwise skip

--- app/services/create_job_by_url.rb
+++ app/services/create_job_by_url.rb
@@ -1,4 +1,6 @@
 class CreateJobByUrl
+  include AtsDetector
+
   def initialize(url)
     @url = url
   end
@@ -6,19 +8,23 @@ class CreateJobByUrl
   def call
     ats, ats_identifier, job_id = ParseJobUrlByAts.new(@url).parse
 
-    company = ats.find_or_create_company(ats_identifier)
+    if SUPPORTED_ATS_SYSTEMS.include?(ats.name)
+      company = ats.find_or_create_company(ats_identifier)
 
-    puts "Created company - #{company.company_name}" if company.persisted?
+      puts "Created company - #{company.company_name}" if company.persisted?
 
-    # TODO: To be updated
-    # 1. If company is new record, needs to get the company details from the API
-    # 2. If company is not a new record, no need to do anything
-    # 3. This is CompanyCreator functionality from before
+      # TODO: To be updated
+      # 1. If company is new record, needs to get the company details from the API
+      # 2. If company is not a new record, no need to do anything
+      # 3. This is CompanyCreator functionality from before
 
-    job = ats.find_or_create_job_by_id(company, job_id) if job_id
+      job = ats.find_or_create_job_by_id(company, job_id) if job_id
 
-    puts "Created job - #{job.job_title}" if job.persisted?
+      puts "Created job - #{job.job_title}" if job.persisted?
 
-    [ats, company, job]
+      [ats, company, job]
+    else
+      ats
+    end
   end
 end
