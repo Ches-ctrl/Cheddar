72fda5dccec98e72674b67e8c35acf5c9f882f5c
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-04-14 18:46:01 +0100

    adding logic that matches create_job_from_url

--- app/services/import_companies_from_csv.rb
+++ app/services/import_companies_from_csv.rb
@@ -14,15 +14,31 @@ class ImportCompaniesFromCsv
   def call
     @company_urls.each do |url|
       # TODO: Fix this. At the moment we have multiple ways of importing companies into the DB. We should just have one format.
-      ats, ats_identifier, job_id = ParseJobUrlByAts.new(url).parse(@companies)
-      next puts "couldn't find ats for url: #{url}" unless ats
-      next puts "invalid identifier: #{ats_identifier}" unless ats_identifier
+      # TODO: Move this into a module or separate service object e.g. CreateCompany or CreateCompanyFromUrl as this is replicated code from CreateJobFromUrl and is the same logic
+
+      ats = ApplicantTrackingSystem.determine_ats(@url)
+      p "ATS - #{ats.name}"
+
+      # ---------------
+      # Parse URL
+      # ---------------
+
+      ats_identifier, job_id = ApplicantTrackingSystem.parse_url(ats, @url)
+      p "ATS Identifier - #{ats_identifier}"
+      p "Job ID - #{job_id}"
+
+      # ---------------
+      # CompanyCreator
+      # ---------------
+
+      company = ApplicantTrackingSystem.get_company_details(@url, ats, ats_identifier)
+      puts "Created company - #{company.company_name}"
 
       # disable job and company creation to save time
-      next puts "invalid identifier: #{ats_identifier}" unless ats_identifier && (company = ats.find_or_create_company(ats_identifier))
+      # next puts "invalid identifier: #{ats_identifier}" unless ats_identifier && (company = ats.find_or_create_company(ats_identifier))
 
-      @companies[ats.name] << ats_identifier
-      puts "unable to create job with: #{url}" unless job_id && ats.find_or_create_job_by_id(company, job_id)
+      # @companies[ats.name] << ats_identifier
+      # puts "unable to create job with: #{url}" unless job_id && ats.find_or_create_job_by_id(company, job_id)
     end
 
     puts "\nCreated #{Job.count - @no_of_jobs} new jobs with #{Company.count - @no_of_companies} new companies."
