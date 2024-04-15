436d553b8f79f14f0c69ffbe7612d753e87de632
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-04-15 06:57:05 +0100

    refactoring create_company_from_url to have one url and be usable elsewhere in the codebase

--- app/services/url/create_company_from_url.rb
+++ app/services/url/create_company_from_url.rb
@@ -1,21 +1,23 @@
-class CreateCompanyFromUrl
-  include CompanyCsv
-  include CheckUrlIsValid
-  include AtsSystemParser
+module Url
+  class CreateCompanyFromUrl
+    include CompanyCsv
+    include CheckUrlIsValid
+    include AtsSystemParser
 
-  def initialize
-    puts "Importing companies from CSV list..."
-    @company_urls = load_from_csv('other_company_urls')
-    @companies = ats_list
-    @no_of_companies = Company.count
-    @no_of_jobs = Job.count
-  end
+    def initialize(url)
+      @url = url
+    end
 
-  def call
-    @company_urls.each do |url|
-      p "URL - #{url}"
+    def create_company
       # TODO: Fix this. At the moment we have multiple ways of importing companies into the DB. We should just have one format.
       # TODO: Move this into a module or separate service object e.g. CreateCompany or CreateCompanyFromUrl as this is replicated code from CreateJobFromUrl and is the same logic
+      # TODO: Not sure how this is meant to work but the refactor killed the ParseJobUrlFromAts class so had to port in other methods here
+
+      # ---------------
+      # ATS Router
+      # ---------------
+
+      p @url
 
       ats = ApplicantTrackingSystem.determine_ats(@url)
       p "ATS - #{ats.name}"
@@ -32,18 +34,18 @@ class CreateCompanyFromUrl
       # CompanyCreator
       # ---------------
 
-      company = ApplicantTrackingSystem.get_company_details(@url, ats, ats_identifier)
+      company = ApplicantTrackingSystem.get_company_details(url, ats, ats_identifier)
       puts "Created company - #{company.company_name}"
 
-      # disable job and company creation to save time
-      # next puts "invalid identifier: #{ats_identifier}" unless ats_identifier && (company = ats.find_or_create_company(ats_identifier))
+      # ---------------
+      # SaveToCsv
+      # ---------------
 
       # @companies[ats.name] << ats_identifier
       # puts "unable to create job with: #{url}" unless job_id && ats.find_or_create_job_by_id(company, job_id)
-    end
 
-    puts "\nCreated #{Job.count - @no_of_jobs} new jobs with #{Company.count - @no_of_companies} new companies."
-    puts "\nStoring the information in CSV format..."
-    save_ats_list
+      # puts "\nStoring the information in CSV format..."
+      # save_ats_list
+    end
   end
 end
