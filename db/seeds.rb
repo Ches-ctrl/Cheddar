ccc9e10748b72d3a1318a5d8ae7973e644a2c8e1
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-04-15 07:39:26 +0100

    add note for later

--- db/seeds.rb
+++ db/seeds.rb
@@ -73,6 +73,7 @@ puts "\nHow many jobs do you want to seed in the database?\n"
 #   response = gets.chomp
 #   if response == 'run updater'
 #     # Scraper::DevitJob.perform_later
+#     # TODO: Fix CreateCompanyFromUrl as will now need to call the rake task instead (and all of this really needs moving to a controller)
 #     # CreateCompanyFromUrl.new.call
 #     # Xml::WorkableJob.perform_later
 #     # ScrapeTrueUpJob.perform_later
