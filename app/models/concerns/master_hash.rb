e824388af840a5fa005fb4c440d2ff129b6ab83e
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-04-22 11:31:44 +0100

    create master application_criteria hash to reference against with master keys

---
+++ app/models/concerns/master_hash.rb
@@ -0,0 +1,11 @@
+class MasterHash < ApplicationRecord
+end
+
+# application_criteria = {
+#   resume: {
+#     required: true,
+#   },
+# }
+
+# TODO: built master hash to reference against
+# TODO: build dictionary of locators across ATS systems
