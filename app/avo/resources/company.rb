2622b074ef576d3c4eff55c06c1052cdea4945a5
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-04-22 10:04:15 +0100

    reorder avo columns

--- app/avo/resources/company.rb
+++ app/avo/resources/company.rb
@@ -9,18 +9,18 @@ module Avo
       def fields
         field :id, as: :id
         field :name, as: :text
-        field :company_website_url, as: :text
-        field :location, as: :text
-        field :industry, as: :text
-        field :url_careers, as: :text
-        field :url_linkedin, as: :text
-        field :industry_subcategory, as: :text
         field :applicant_tracking_system_id, as: :number
         field :ats_identifier, as: :text
         field :description, as: :textarea
-        field :total_live, as: :number
+        field :company_website_url, as: :text
+        field :url_careers, as: :text
+        field :url_linkedin, as: :text
         field :url_ats_main, as: :text
         field :url_ats_api, as: :text
+        field :location, as: :text
+        field :industry, as: :text
+        field :industry_subcategory, as: :text
+        field :total_live, as: :number
         field :applicant_tracking_system, as: :belongs_to
         field :jobs, as: :has_many
       end
