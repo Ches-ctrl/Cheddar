b568f705b7e30393c7c010dda7edec610c101746
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-04-22 10:18:00 +0100

    removing unnecessary code

--- app/models/concerns/ats/workable/company_details.rb
+++ app/models/concerns/ats/workable/company_details.rb
@@ -22,35 +22,6 @@ module Ats
 
       def check_for_careers_url_redirect(url_ats_main)
       end
-
-      # TODO: If there's a scenario where check_for_details is necessary, call it from ApplicantTrackingSystem
-
-      # def check_for_details(company, ats_system, ats_identifier, description)
-      #   if company.description.nil?
-      #     p "Missing description for #{company.name}"
-      #     company.update(description:)
-      #   end
-
-      #   if company.ats_identifier.nil?
-      #     p "Missing ATS identifier for #{company.name}"
-      #     company.update(ats_identifier:)
-      #   end
-
-      #   if company.applicant_tracking_system_id.nil?
-      #     p "Missing ATS system for #{company.name}"
-      #     company.update(applicant_tracking_system_id: ats_system.id)
-      #   end
-
-      #   if company.url_ats_api.nil?
-      #     p "Missing ATS API URL for #{company.name}"
-      #     company.update(url_ats_api: "#{ats_system.url_api}#{ats_identifier}")
-      #   end
-
-      #   return unless company.url_ats_main.nil?
-
-      #   p "Missing ATS Main URL for #{company.name}"
-      #   company.update(url_ats_main: "#{ats_system.url_base}#{ats_identifier}")
-      # end
     end
   end
 end
