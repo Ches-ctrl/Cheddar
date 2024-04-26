f3351db9c90a235eb7feea072ed22311668ce83b
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-04-26 18:31:12 +0100

    Remove trailing whitespace in job.rb

--- app/models/job.rb
+++ app/models/job.rb
@@ -154,7 +154,7 @@ class Job < ApplicationRecord
 
   private_class_method def self.filter_by_location(param)
     return unless param.present?
-    
+
     locations = param.split.map { |location| location.gsub('_', ' ').split.map(&:capitalize).join(' ') unless location == 'remote' }
     { city: locations }
   end
