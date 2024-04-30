d6c937411f43c819241672d57ddf51351ce7826a
Daniel Sussman <daniel.o.sussman@gmail.com>
2024-04-30 17:51:42 +0100

    more adjustments to Webmock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,6 +27,8 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/cassettes"
   config.hook_into :webmock
   config.configure_rspec_metadata!
+  config.allow_http_connections_when_no_cassette = true
+  config.ignore_localhost = true
   config.ignore_hosts 'dev.virtualearth.net'
 end
 
