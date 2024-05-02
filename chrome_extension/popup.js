30aa1cc81be2194ec99b157b6e80e6444b9df6c9
EdFage <87755165+EdFage@users.noreply.github.com>
2024-05-02 13:31:02 +1000

    add pseudocode

---
+++ chrome_extension/popup.js
@@ -0,0 +1,11 @@
+async function saveJob() {
+    // Get authentication data
+    // Create new tab and redirect user there if they are not logged in
+
+    // if user is logged in
+        // make api call
+
+        // if api call returns 200 OK, display a "job saved" message
+
+        // else, return an error
+}
