d7944ae6fce0153261c731fd567cf98deab61506
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-05-07 13:00:48 +0100

    job added successfully for now

--- app/controllers/api/v0/jobs_controller.rb
+++ app/controllers/api/v0/jobs_controller.rb
@@ -4,12 +4,13 @@ module Api
       def add_job
         job_url = params[:job_url]
         p job_url
+        render json: { message: 'Job added successfully' }, status: :ok
 
-        if CreateJobFromUrl.call(job_url)
-          render json: { message: 'Job added successfully' }, status: :ok
-        else
-          render json: { error: 'Failed to add job' }, status: :unprocessable_entity
-        end
+        # if CreateJobFromUrl.call(job_url)
+        #   render json: { message: 'Job added successfully' }, status: :ok
+        # else
+        #   render json: { error: 'Failed to add job' }, status: :unprocessable_entity
+        # end
       end
     end
   end
