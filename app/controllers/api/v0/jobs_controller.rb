f42bdc6955935fc3605eba1e20167e9f2bd7fd54
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-05-07 13:33:49 +0100

    add skip_before_actions to CSRF and user login - pending updates later

--- app/controllers/api/v0/jobs_controller.rb
+++ app/controllers/api/v0/jobs_controller.rb
@@ -1,12 +1,16 @@
 module Api
   module V0
     class JobsController < ApplicationController
+      skip_before_action :verify_authenticity_token, only: :add_job
+      skip_before_action :authenticate_user!, only: :add_job
+
       def add_job
-        job_url = params[:job_url]
-        p job_url
+        posting_url = params[:posting_url]
+        p "posting_url: #{posting_url}"
+
         render json: { message: 'Job added successfully' }, status: :ok
 
-        # if CreateJobFromUrl.call(job_url)
+        # if CreateJobFromUrl.call(posting_url)
         #   render json: { message: 'Job added successfully' }, status: :ok
         # else
         #   render json: { error: 'Failed to add job' }, status: :unprocessable_entity
