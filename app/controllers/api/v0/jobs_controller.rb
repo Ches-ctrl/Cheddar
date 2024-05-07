2305b69f62a2952fd962f5e4782ed5d1973a1e6e
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-05-07 13:00:17 +0100

    add method to process job url

---
+++ app/controllers/api/v0/jobs_controller.rb
@@ -0,0 +1,16 @@
+module Api
+  module V0
+    class JobsController < ApplicationController
+      def add_job
+        job_url = params[:job_url]
+        p job_url
+
+        if CreateJobFromUrl.call(job_url)
+          render json: { message: 'Job added successfully' }, status: :ok
+        else
+          render json: { error: 'Failed to add job' }, status: :unprocessable_entity
+        end
+      end
+    end
+  end
+end
