46dfae5edefb0d62fb377ce427e1aa8cecf4626d
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-05-08 22:39:49 +0100

    rename to net_zero_data

--- app/services/net_zero/net_zero_api.rb
+++
@@ -1,4 +0,0 @@
-module NetZero
-  class NetZeroApi
-  end
-end
--- app/services/net_zero_data/zero_tracker.rb
+++ app/services/net_zero_data/zero_tracker.rb
@@ -1,29 +1,6 @@
-module NetZero
+module NetZeroData
   class ZeroTracker
     require 'net/http'
-
-    # Pseudocode
-    # Setup models for the Net Zero data providers
-    # Setup a main class that calls each scraper module as a sub-class
-    # Get all companies from each API to see what data is available
-    # Sources: Zero Tracker, EUNZDP, SBTi, manual data
-    # Setup a background job that calls the service class
-
-    # Later
-    # Mock the API calls for testing
-    # Error handling - what to do if the API call fails?
-    # Monitoring source of the data to see % allocation
-    # Setup background job to update quarterly (UpdateExistingNetZeroDataJob)
-    # Display this data in a separate page using a standard DB structure as well as chartjs
-
-    # Notes
-    # Likely that there are company repetitions across the various sources
-
-    # Questions
-    # Need to create a unique identifier for each company so that it gets the correct data from the APIs?
-    # How to de-dupe companies?
-    # How to uniquely identify a company?
-    # Assigning industry and sub-industry to the companies?
 
     def self.perform
       url = URI("https://zerotracker.net/api/v1/companies")
