b96e24c34ebe9e768e038eddfcd53471ea961364
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-05-09 12:17:20 +0100

    add explanatory notes

--- app/services/net_zero_data/net_zero_api.rb
+++ app/services/net_zero_data/net_zero_api.rb
@@ -2,6 +2,22 @@ module NetZeroData
   class NetZeroApi
     require 'net/http'
 
+    # Notes:
+    # List of Net Zero Data Sources: Zero Tracker, EU NZDP, SBTi, CDP, manual data (in order of priority)
+    # Think we should match companies so that they're unique based on their ISIN (prevents likely duplication across multiple sources)
+    # Note: SBTi, CDP and manual data do not have public APIs so must be built from CSVs
+    # Note: GCAP (UNFCCC) is a wrapper on the CDP dataset (could be scraped but limited info available)
+
+    # To do later:
+    # Mock the API calls for testing
+    # Error handling - what to do if the API call fails?
+    # Monitoring source of the data to see % allocation
+    # Setup background job to update quarterly (UpdateExistingNetZeroDataJob)
+    # Display this data in a separate page using a standard DB structure as well as chartjs
+
+    # Questions:
+    # Assigning industry and sub-industry to the companies?
+
     def self.build_all_data(base_url)
       url = URI(base_url)
       companies_data = fetch_json_data(url)
@@ -49,28 +65,3 @@ module NetZeroData
     end
   end
 end
-
-# Pseudocode
-# Setup models for the Net Zero data providers
-# Setup a main class that calls each scraper module as a sub-class
-# Get all companies from each API to see what data is available
-# Sources: Zero Tracker, EUNZDP, SBTi, CDP, manual data
-# Note: SBTi & CDP do not have public APIs
-# Note: GCAP (UNFCCC) is a wrapper on the CDP dataset
-# Setup a background job that calls the service class
-
-# Later
-# Mock the API calls for testing
-# Error handling - what to do if the API call fails?
-# Monitoring source of the data to see % allocation
-# Setup background job to update quarterly (UpdateExistingNetZeroDataJob)
-# Display this data in a separate page using a standard DB structure as well as chartjs
-
-# Notes
-# Likely that there are company repetitions across the various sources
-
-# Questions
-# Need to create a unique identifier for each company so that it gets the correct data from the APIs?
-# How to de-dupe companies?
-# How to uniquely identify a company?
-# Assigning industry and sub-industry to the companies?
