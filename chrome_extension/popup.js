c3fc9bc72cee048733c1b5d73df1ed070fe21f77
EdFage <87755165+EdFage@users.noreply.github.com>
2024-05-10 14:33:00 +1000

    add function to get current tab and make api call, update to avoid inline javascript

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,11 +1,55 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var saveButton = document.getElementById('saveJobButton');
+    saveButton.addEventListener('click', saveJob);
+});
+
 async function saveJob() {
-    // Get authentication data
-    // Create new tab and redirect user there if they are not logged in
+    // get current job url
 
-    // if user is logged in
-        // make api call
+    var currentTabUrl = await getCurrentTab();
 
-        // if api call returns 200 OK, display a "job saved" message
-
-        // else, return an error
+    // make api call
+    jobAPIcall("", currentTabUrl, "http://127.0.0.1:3000/api/v0/add_job")
+        .then(response => response.json()) // Transform the response to JSON
+        .then(data => {
+            console.log(data); // Handle the data from the response
+        })
+        .catch(error => {
+            console.error('Error:', error); // Handle any errors
+        });
 }
+
+/**
+ * 
+ * @returns {string} the url of the current tab
+ * 
+ */
+async function getCurrentTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab.url;
+}
+
+
+/**
+ * 
+ * @param {string} csrfToken 
+ * @param {string} job_url: the job to be saved
+ * @param {string} api_url: the url of the api
+ * @returns a promise from the api
+ * 
+ */
+function jobAPIcall(csrfToken, job_url, api_url) {
+    return fetch(api_url, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrfToken // Include the CSRF token here
+        },
+        body: JSON.stringify({
+            posting_url: job_url
+        })
+    })
+}
+
+
