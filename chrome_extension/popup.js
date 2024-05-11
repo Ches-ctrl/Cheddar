f8009aaef2e3a630a7dbbebfd388b73920be5f07
EdFage <87755165+EdFage@users.noreply.github.com>
2024-05-11 17:11:40 +1000

    update getCurrentTab

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -26,7 +26,7 @@ async function saveJob() {
  */
 async function getCurrentTab() {
     let queryOptions = { active: true, lastFocusedWindow: true };
-    let [tab] = await chrome.tabs.query(queryOptions);
+    let tab = await chrome.tabs.query(queryOptions);
     return tab.url;
 }
 
