eb526028a910f924bfa21eaa4ad697a3c5b532d6
EdFage <87755165+EdFage@users.noreply.github.com>
2024-05-11 17:14:16 +1000

    add development extension ID

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'chrome-extension://hofjpmhpciiodhklobchgedbbglhjboa'
+    origins 'chrome-extension://hofjpmhpciiodhklobchgedbbglhjboa', 'chrome-extension://ioffpdelmlddmepimkcadfmmhfhocgkm' 
     resource '/api/v0/add_job', headers: :any, methods: [:post]
   end
 end
