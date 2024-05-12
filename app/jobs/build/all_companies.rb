6e6ac044a9f7e01fceb90a517d6348388c5ed67d
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-05-12 08:26:06 +0100

    delete badly named files

--- app/jobs/build/all_companies.rb
+++
@@ -1,9 +0,0 @@
-module Build
-  class AllCompanies < ApplicationJob
-    queue_as :updates
-
-    def perform
-      CompanyBuilder.new.perform
-    end
-  end
-end
--- app/jobs/build/all_jobs.rb
+++
@@ -1,9 +0,0 @@
-module Build
-  class AllJobs < ApplicationJob
-    queue_as :updates
-
-    def perform
-      Build::AllJobs.new.perform
-    end
-  end
-end
