d67afaf1e00f7b92f3f584d45d35cef12f96f898
EdFage <87755165+EdFage@users.noreply.github.com>
2024-05-14 08:47:00 +1000

    remove old development extension ID

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'chrome-extension://hofjpmhpciiodhklobchgedbbglhjboa', 'chrome-extension://ioffpdelmlddmepimkcadfmmhfhocgkm' 
+    origins 'chrome-extension://hofjpmhpciiodhklobchgedbbglhjboa' 
     resource '/api/v0/add_job', headers: :any, methods: [:post]
   end
 end
