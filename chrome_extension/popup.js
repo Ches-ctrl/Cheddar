cf56fe8b9aba262fc341a74ef8d6607d80a70244
EdFage <87755165+EdFage@users.noreply.github.com>
2024-05-19 11:55:14 +1000

    update api call logic

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,25 +1,57 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var saveButton = document.getElementById('saveJobButton');
     saveButton.addEventListener('click', saveJob);
 });
 
 async function saveJob() {
+
     // get current job url
+    cheddarUrl = "http://127.0.0.1:3000/"
+
 
     var currentTabUrl = await getCurrentTab();
     console.log("currentTabUrl:", currentTabUrl);
 
     // make api call
-    jobAPIcall("", currentTabUrl, "http://127.0.0.1:3000/api/v0/add_job")
-        .then(response => response.json()) // Transform the response to JSON
-        .then(data => {
-            console.log(data); // Handle the data from the response
+    console.log(chrome.cookies.get({ url: cheddarUrl, name: "_cheddar_session" }));
+
+    const myHeaders = new Headers();
+    myHeaders.append("x-api-key", "b1e30ea0862236d562de386d5fb9184292956c");
+
+    const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        redirect: "follow",
+        credentials: "include"
+    };
+
+    fetch(`${cheddarUrl}/api/v0/add_job?posting_url=${currentTabUrl}`, requestOptions)
+        .then((response) => {
+            console.log("response: ", response)
+            console.log("response text: ", response.text());
+            if (!response.ok) {
+                throw new Error('Unexpected response status: ' + response.status);
+            }
+            if (response.status == 200) {
+                if (response.redirected == true) {
+                    chrome.tabs.create({
+                        url: response.url
+                    })
+                }
+                else {
+                    const messageElement = document.getElementById('message');
+                    messageElement.style.display = 'block';
+                    setTimeout(() => {
+                        messageElement.style.display = 'none';
+                    }, 15000);
+                }
+            }
         })
-        .catch(error => {
-            console.error('Error:', error); // Handle any errors
-        });
+        .catch((error) => console.error(error));
 
 }
+
+
 
 /**
  * 
@@ -31,28 +63,6 @@ async function getCurrentTab() {
     // `tab` will either be a `tabs.Tab` instance or `undefined`.
     let [tab] = await chrome.tabs.query(queryOptions);
     return tab.url;
-  }
-
-
-/**
- * 
- * @param {string} csrfToken 
- * @param {string} job_url: the job to be saved
- * @param {string} api_url: the url of the api
- * @returns a promise from the api
- * 
- */
-function jobAPIcall(csrfToken, job_url, api_url) {
-    return fetch(api_url, {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json',
-            'x-api-key': 'b1e30ea0862236d562de386d5fb9184292956c',
-        },
-        body: JSON.stringify({
-            posting_url: job_url
-        })
-    })
 }
 
 
