1f8d269052c5f96dac3142860a7b2136f0e435ac
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-05-19 15:24:33 +0100

    turn off hubspot for now

--- app/services/email/hubspot.rb
+++ app/services/email/hubspot.rb
@@ -1,28 +1,28 @@
 module Email
-  class Hubspot
-    require 'hubspot-api-client'
+  class Hubspot # rubocop:disable Lint/EmptyClass
+    # require 'hubspot-api-client'
 
-    def initialize
-      @client = Hubspot::Client.new(access_token: ENV.fetch('HUBSPOT_API_KEY'))
-    end
+    # def initialize
+    #   @client = Hubspot::Client.new(access_token: ENV.fetch('HUBSPOT_API_KEY'))
+    # end
 
-    def add_contact(email)
-      contact_properties = {
-        properties: {
-          email:
-        }
-      }
+    # def add_contact(email)
+    #   contact_properties = {
+    #     properties: {
+    #       email:
+    #     }
+    #   }
 
-      begin
-        response = @client.crm.contacts.basic_api.create(contact_properties)
-        p "HubSpot contact created: #{response.to_json}"
-        Rails.logger.info("HubSpot contact created: #{response.to_json}")
-        true
-      rescue Hubspot::ApiError => e
-        p "Error creating HubSpot contact: #{e.response_body}"
-        Rails.logger.error("Error creating HubSpot contact: #{e.response_body}")
-        false
-      end
-    end
+    #   begin
+    #     response = @client.crm.contacts.basic_api.create(contact_properties)
+    #     p "HubSpot contact created: #{response.to_json}"
+    #     Rails.logger.info("HubSpot contact created: #{response.to_json}")
+    #     true
+    #   rescue Hubspot::ApiError => e
+    #     p "Error creating HubSpot contact: #{e.response_body}"
+    #     Rails.logger.error("Error creating HubSpot contact: #{e.response_body}")
+    #     false
+    #   end
+    # end
   end
 end
