fcf0880fef64d01a03b63b1e4883b832eacb6b35
EdFage <87755165+EdFage@users.noreply.github.com>
2024-05-19 18:54:09 +1000

    format

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,19 +1,19 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var saveButton = document.getElementById('saveJobButton');
-    saveButton.addEventListener('click', saveJob);
+document.addEventListener("DOMContentLoaded", function () {
+    var saveButton = document.getElementById("saveJobButton");
+    saveButton.addEventListener("click", saveJob);
 });
 
 async function saveJob() {
-
     // get current job url
-    cheddarUrl = "http://127.0.0.1:3000/"
-
+    cheddarUrl = "http://127.0.0.1:3000/";
 
     var currentTabUrl = await getCurrentTab();
     console.log("currentTabUrl:", currentTabUrl);
 
     // make api call
-    console.log(chrome.cookies.get({ url: cheddarUrl, name: "_cheddar_session" }));
+    console.log(
+        chrome.cookies.get({ url: cheddarUrl, name: "_cheddar_session" })
+    );
 
     const myHeaders = new Headers();
     myHeaders.append("x-api-key", "b1e30ea0862236d562de386d5fb9184292956c");
@@ -22,41 +22,42 @@ async function saveJob() {
         method: "POST",
         headers: myHeaders,
         redirect: "follow",
-        credentials: "include"
+        credentials: "include",
     };
 
-    fetch(`${cheddarUrl}/api/v0/add_job?posting_url=${currentTabUrl}`, requestOptions)
+    fetch(
+        `${cheddarUrl}/api/v0/add_job?posting_url=${currentTabUrl}`,
+        requestOptions
+    )
         .then((response) => {
-            console.log("response: ", response)
+            console.log("response: ", response);
             console.log("response text: ", response.text());
             if (!response.ok) {
-                throw new Error('Unexpected response status: ' + response.status);
+                throw new Error(
+                    "Unexpected response status: " + response.status
+                );
             }
             if (response.status == 200) {
                 if (response.redirected == true) {
                     chrome.tabs.create({
-                        url: response.url
-                    })
-                }
-                else {
-                    const messageElement = document.getElementById('message');
-                    messageElement.style.display = 'block';
+                        url: response.url,
+                    });
+                } else {
+                    const messageElement = document.getElementById("message");
+                    messageElement.style.display = "block";
                     setTimeout(() => {
-                        messageElement.style.display = 'none';
+                        messageElement.style.display = "none";
                     }, 15000);
                 }
             }
         })
         .catch((error) => console.error(error));
-
 }
 
-
-
 /**
- * 
+ *
  * @returns {string} the url of the current tab
- * 
+ *
  */
 async function getCurrentTab() {
     let queryOptions = { active: true, lastFocusedWindow: true };
@@ -64,5 +65,3 @@ async function getCurrentTab() {
     let [tab] = await chrome.tabs.query(queryOptions);
     return tab.url;
 }
-
-
