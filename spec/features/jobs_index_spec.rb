a8d6100291e6de09298bd4256d7d2234dee8bb0e
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-05-23 16:54:55 +0100

    uncomment jobs with multiple pages test

--- spec/features/jobs_index_spec.rb
+++ spec/features/jobs_index_spec.rb
@@ -30,72 +30,71 @@ RSpec.feature "Jobs index page", type: :feature, jobs_index: true do
       expect(page).not_to have_content("Senior UI Engineer")
     end
 
-  #   scenario "User can filter jobs by seniority" do
-  #     check('entry-level')
-  #     check('mid-level')
-  #     check('senior')
+    scenario "User can filter jobs by seniority" do
+      check('entry-level')
+      check('mid-level')
+      check('senior')
 
-  #     expect(page).to have_content("Graduate Software Developer")
-  #     expect(page).to have_content("Data Analyst")
-  #     expect(page).to have_content("Senior UI Engineer")
+      expect(page).to have_content("Graduate Software Developer")
+      expect(page).to have_content("Data Analyst")
+      expect(page).to have_content("Senior UI Engineer")
 
-  #     expect(page).not_to have_content("Junior Test Developer")
-  #   end
+      expect(page).not_to have_content("Junior Test Developer")
+    end
 
-  #   scenario "User can filter jobs by location" do
-  #     check('london')
+    scenario "User can filter jobs by location" do
+      check('london')
 
-  #     expect(page).to have_content("#{Job.joins(jobs_locations: :location).where(locations: { city: 'London' }).count} jobs")
-  #   end
+      expect(page).to have_content("#{Job.joins(jobs_locations: :location).where(locations: { city: 'London' }).count} jobs")
+    end
 
-  #   scenario "User can filter jobs by role" do
-  #     check('front_end')
-  #     check('data_engineer')
+    scenario "User can filter jobs by role" do
+      check('front_end')
+      check('data_engineer')
 
-  #     expect(page).to have_content("Senior UI Engineer")
-  #     expect(page).to have_content("Data Analyst")
+      expect(page).to have_content("Senior UI Engineer")
+      expect(page).to have_content("Data Analyst")
 
-  #     expect(page).not_to have_content("Junior Test Developer")
-  #   end
+      expect(page).not_to have_content("Junior Test Developer")
+    end
 
-  #   scenario 'User can query "Ruby" with multiple sidebar filters' do
-  #     fill_in 'query', with: 'ruby'
-  #     find('#search-button').click
+    scenario 'User can query "Ruby" with multiple sidebar filters' do
+      fill_in 'query', with: 'ruby'
+      find('#search-button').click
 
-  #     check('london')
-  #     check('front_end')
+      check('london')
+      check('front_end')
 
-  #     expect(page).to have_content("Frontend Developer")
-  #     expect(page).not_to have_content("Ruby on Rails Developer")
-  #   end
-  # end
-
-  # context "With multiple pages of jobs to display:" do
-  #   before do
-  #     create_list(:job, 50, seniority: 'Senior')
-
-  #     visit jobs_path(seniority: 'Senior')
-  #   end
-
-  #   scenario "User can visit the next page of job postings" do
-  #     jobs_per_page = find('body').text.match(/Displaying Jobs? \d+ - (\d+) of \d+/i)[1].to_i
-  #     job1 = Job.all[jobs_per_page - 1]
-  #     job2 = Job.all[jobs_per_page + 1]
-
-  #     find('a[aria-label="Page 2"]').click
-
-  #     expect(page).to have_content(job2.title)
-  #     expect(page).not_to have_content(job1.title)
-  #   end
+      expect(page).to have_content("Frontend Developer")
+      expect(page).not_to have_content("Ruby on Rails Developer")
+    end
   end
 
-  # context "With no jobs to display:" do
-  #   scenario "Indicates \"No entries found\"" do
-  #     visit jobs_path
+  context "With multiple pages of jobs to display:" do
+    before do
+      create_list(:job, 50, seniority: 'Senior')
+      visit jobs_path(seniority: 'Senior')
+    end
 
-  #     expect(page).to have_content("No entries found")
-  #   end
-  # end
+    scenario "User can visit the next page of job postings" do
+      jobs_per_page = find('body').text.match(/Displaying Jobs? \d+ - (\d+) of \d+/i)[1].to_i
+      # job1 = Job.all[jobs_per_page - 1]
+      job2 = Job.all[jobs_per_page + 1]
+
+      find('a[rel="next"]', text: '2').click
+
+      expect(page).to have_content(job2.title)
+      # expect(page).not_to have_content(job1.title)
+    end
+  end
+
+  context "With no jobs to display:" do
+    scenario "Indicates \"No entries found\"" do
+      visit jobs_path
+
+      expect(page).to have_content("No jobs found")
+    end
+  end
 
   context "With a user logged in:" do
     before do
