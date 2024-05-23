f2bf3298e77bbd3a24fdc0f5428a16b05c23bbbe
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-05-23 18:20:36 +0100

    comment out headless as was

--- config/initializers/capybara.rb
+++ config/initializers/capybara.rb
@@ -16,8 +16,8 @@ Capybara.register_driver :selenium do |app|
   else
     options = Selenium::WebDriver::Chrome::Options.new
 
-    options.add_argument("--headless")
-    options.add_argument("--disable-gpu")
+    # options.add_argument("--headless")
+    # options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--window-size=1400,1000")
     options.add_argument("--disable-dev-shm-usage")
