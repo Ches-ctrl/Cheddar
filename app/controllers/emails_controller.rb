900d97b13a892d88044a57b84975ecd50061a8e3
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-05-23 14:30:37 +0100

    adding in working hubspot - finally got there with the rubydocs

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -2,14 +2,15 @@ class EmailsController < ApplicationController
   def create
     email = params[:email]
 
-    # TODO: Integrate Hubspot via OAuth - at the moment we're using Sendgrid and this doesn't work
-    # hubspot_service = Email::Hubspot.new
+    # TODO: Update handling of emails that have already been signed up - notify user
+
+    hubspot_service = Email::Hubspot.new
     sendgrid_service = Email::Sendgrid.new
 
-    # hubspot_response = hubspot_service.add_contact(email)
+    hubspot_response = hubspot_service.add_contact(email)
     sendgrid_response = sendgrid_service.add_contact(email)
 
-    if sendgrid_response.status_code.to_i == 202
+    if sendgrid_response.status_code.to_i == 202 && hubspot_response
       sendgrid_service.send_confirmation_email(email)
       redirect_to root_path, notice: 'Subscription successful. Please check your email for confirmation.'
     else
