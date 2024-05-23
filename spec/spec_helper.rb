ac606f4a6682ba23ee7475efd3ead943b224b7d9
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-05-23 16:54:38 +0100

    change vcr to only record once

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,7 +26,7 @@ end
 
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/cassettes"
-  config.default_cassette_options = { :record => :new_episodes }
+  config.default_cassette_options = { :record => :once }
   config.hook_into :webmock
   config.configure_rspec_metadata!
   config.allow_http_connections_when_no_cassette = true
