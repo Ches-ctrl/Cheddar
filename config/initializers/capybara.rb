6009aae57730c0fc91bdceaa029cd03694c30508
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-05-23 18:19:16 +0100

    tidy up capybara initialiser

--- config/initializers/capybara.rb
+++ config/initializers/capybara.rb
@@ -16,16 +16,18 @@ Capybara.register_driver :selenium do |app|
   else
     options = Selenium::WebDriver::Chrome::Options.new
 
-    # options.add_argument("--headless")
-    # options.add_argument("--disable-gpu")
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--window-size=1400,1000")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--remote-debugging-port=9225")
+
     Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
   end
 end
 
-Capybara.ignore_hidden_elements = false
+# Global settings
 Capybara.default_driver = :selenium
 Capybara.javascript_driver = :selenium
+Capybara.ignore_hidden_elements = false
