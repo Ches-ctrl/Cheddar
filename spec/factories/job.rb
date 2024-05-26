36e2085a9bc858c957e855bb0f7fe115bf182afb
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-05-26 08:16:41 +0100

    initialiaze with

--- spec/factories/job.rb
+++ spec/factories/job.rb
@@ -5,46 +5,48 @@ FactoryBot.define do
     sequence(:posting_url) { |n| "https://www.example-#{n}.com/jobs/1" }
     roles { Array.new(rand(1..3)) { association :role } }
 
-    trait :entry_level_mobile do
-      seniority { 'Entry-Level' }
-      after(:build) do |job|
-        job.roles << Role.find_or_create_by(name: 'mobile')
-      end
-    end
+    initialize_with { new(attributes) }
+  end
 
-    trait :junior_dev_ops do
-      seniority { 'Junior' }
-      after(:build) do |job|
-        job.roles << Role.find_or_create_by(name: 'mobile')
-      end
+  trait :entry_level_mobile do
+    seniority { 'Entry-Level' }
+    after(:build) do |job|
+      job.roles << Role.find_or_create_by(name: 'mobile')
     end
+  end
 
-    trait :mid_level_data do
-      seniority { 'Mid-Level' }
-      after(:build) do |job|
-        job.roles << Role.find_or_create_by(name: 'data_engineer')
-      end
+  trait :junior_dev_ops do
+    seniority { 'Junior' }
+    after(:build) do |job|
+      job.roles << Role.find_or_create_by(name: 'mobile')
     end
+  end
 
-    trait :senior_front_end do
-      seniority { 'Senior' }
-      after(:build) do |job|
-        job.roles << Role.find_or_create_by(name: 'front_end')
-      end
+  trait :mid_level_data do
+    seniority { 'Mid-Level' }
+    after(:build) do |job|
+      job.roles << Role.find_or_create_by(name: 'data_engineer')
     end
+  end
 
-    trait :ruby_front_end do
-      description { 'Ruby on Rails' }
-      after(:build) do |job|
-        job.roles << Role.find_or_create_by(name: 'front_end')
-      end
+  trait :senior_front_end do
+    seniority { 'Senior' }
+    after(:build) do |job|
+      job.roles << Role.find_or_create_by(name: 'front_end')
     end
+  end
 
-    trait :in_london do
-      after(:create) do |job, _evaluator|
-        london = Location.find_by(city: 'London') || create(:location, city: 'London')
-        create(:jobs_location, job:, location: london)
-      end
+  trait :ruby_front_end do
+    description { 'Ruby on Rails' }
+    after(:build) do |job|
+      job.roles << Role.find_or_create_by(name: 'front_end')
+    end
+  end
+
+  trait :in_london do
+    after(:create) do |job, _evaluator|
+      london = Location.find_by(city: 'London') || create(:location, city: 'London')
+      create(:jobs_location, job:, location: london)
     end
   end
 end
