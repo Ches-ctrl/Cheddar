54ed5af541c92f06f277ddfa83307f83a7231908
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-05-26 09:05:41 +0100

    remove page_doc

--- app/services/application_service.rb
+++ app/services/application_service.rb
@@ -1,9 +1,5 @@
 require 'open-uri'
 
 class ApplicationService
-  def page_doc(url)
-    # url = 'https://www.monster.com/jobs/q-it-jobs'
-    data = URI.parse(url).open
-    Nokogiri::XML(data)
-  end
+  # Add service methods here
 end
