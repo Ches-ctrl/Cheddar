3c000bcaeca327ee541fbf3212a4f58ebf593b59
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-05-26 08:47:23 +0100

    delete monster job and service

--- app/jobs/scraper/monster_job.rb
+++
@@ -1,9 +0,0 @@
-module Scraper
-  class MonsterJob < ApplicationJob
-    queue_as :default
-
-    def perform(*_args)
-      Scraper::MonsterService.new.scrape_page
-    end
-  end
-end
--- spec/jobs/scraper/monster_scraper_job_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Scraper::MonsterJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
