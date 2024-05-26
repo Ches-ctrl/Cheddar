15152c98c0d80c2ceb2581852c2b11c9a4f70f92
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-05-26 14:50:14 +0100

    refactor all the code into separate CompanyCreator and JobCreator concerns

--- app/models/concerns/ats/company_creator.rb
+++ app/models/concerns/ats/company_creator.rb
@@ -1,4 +1,54 @@
 module Ats
   module CompanyCreator
+    extend ActiveSupport::Concern
+
+    def find_or_create_company_by_data(data)
+      ats_identifier = fetch_company_id(data)
+      find_or_create_company(ats_identifier, data)
+    end
+
+    def find_or_create_company(ats_identifier, data = nil)
+      return unless ats_identifier
+
+      company = Company.find_or_initialize_by(ats_identifier:) do |new_company|
+        company_data = company_details(ats_identifier)
+
+        new_company.applicant_tracking_system = self
+        new_company.assign_attributes(company_data)
+      end
+
+      if data
+        supplementary_data = company_details_from_data(data)
+        company.assign_attributes(supplementary_data)
+      end
+
+      p "Company created - #{company.name}" if company.new_record? && company.save
+
+      return company
+    end
+
+    def company_details(ats_identifier, data = nil)
+      refer_to_module(defined?(super) ? super : nil, __method__)
+    end
+
+    private
+
+    def company_details_from_data(data)
+      refer_to_module(defined?(super) ? super : nil, __method__)
+    end
+
+    def fetch_company_id(data)
+      refer_to_module(defined?(super) ? super : nil, __method__)
+    end
+
+    def fetch_company_name(ats_identifier)
+      url = "https://autocomplete.clearbit.com/v1/companies/suggest?query=#{ats_identifier}"
+      data = get_json_data(url)
+      return data.dig(0, 'name') unless data.blank?
+    end
+
+    def fetch_total_live(ats_identifier)
+      fetch_company_jobs(ats_identifier)&.count
+    end
   end
 end
--- app/models/concerns/ats/job_creator.rb
+++ app/models/concerns/ats/job_creator.rb
@@ -1,4 +1,75 @@
 module Ats
   module JobCreator
+    extend ActiveSupport::Concern
+
+    def find_or_create_job_by_data(company, data)
+      ats_job_id = fetch_id(data)
+      find_or_create_job(company, ats_job_id, data)
+    end
+
+    def find_or_create_job(company, ats_job_id, data = nil)
+      return unless company&.persisted?
+
+      job = Job.find_or_create_by(ats_job_id:) do |new_job|
+        new_job.company = company
+        new_job.applicant_tracking_system = self
+        new_job.api_url = job_url_api(url_api, company.ats_identifier, ats_job_id)
+        data ||= fetch_job_data(new_job)
+        return if data.blank? || data['error'].present? || data.values.include?(404)
+
+        job_details(new_job, data)
+        fetch_additional_fields(new_job, data)
+        puts "Created new job - #{new_job.title} with #{company.name}"
+      end
+
+      return job
+    end
+
+    # This checks whether job_data is fetched from a job-specific endpoint.
+    # Return false with Lever because its job-specific endpoint gives no additional data on top of
+    # what the company endpoint gives.
+    def individual_job_endpoint_exists?
+      return false if name == 'Lever'
+
+      parameters = method(:job_url_api).parameters
+      parameters.any? { |param| param[1] == :job_id }
+    end
+
+    def fetch_company_jobs(ats_identifier)
+      refer_to_module(defined?(super) ? super : nil, __method__)
+    end
+
+    def fetch_title_and_location(data)
+      refer_to_module(defined?(super) ? super : nil, __method__)
+    end
+
+    def mark_job_expired(job)
+      p "Job with ID #{job.ats_job_id} is expired."
+      job.live = false
+      return nil
+    end
+
+    private
+
+    def job_details(new_job, data)
+      refer_to_module(defined?(super) ? super : nil, __method__)
+    end
+
+    def job_url_api(url_api, ats_identifier, ats_job_id)
+      refer_to_module(defined?(super) ? super : nil, __method__)
+    end
+
+    def fetch_job_data(job)
+      get_json_data(job.api_url)
+    end
+
+    def fetch_additional_fields(job, data)
+      p job
+      get_application_criteria(job, data)
+      p "Getting form fields for #{job.title}..."
+      job.save! # must save before passing to Sidekiq job
+      # TODO: create separate module methods for this
+      GetFormFieldsJob.perform_later(job) if Flipper.enabled?(:get_form_fields)
+    end
   end
 end
