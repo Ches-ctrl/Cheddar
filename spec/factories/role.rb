6a89069eb68d9d0fe53112cd18711f7e9e29993a
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-05-26 07:50:02 +0100

    add roles

--- spec/factories/role.rb
+++ spec/factories/role.rb
@@ -1,5 +1,21 @@
 FactoryBot.define do
   factory :role do
     sequence(:name) { |n| "Role-#{n}" }
+
+    trait :mobile do
+      name { 'mobile' }
+    end
+
+    trait :dev_ops do
+      name { 'dev_ops' }
+    end
+
+    trait :data_engineer do
+      name { 'data_engineer' }
+    end
+
+    trait :front_end do
+      name { 'front_end' }
+    end
   end
 end
