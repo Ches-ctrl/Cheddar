768fab6fe773db1d38ceaa85cfe9df41bd473e1a
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-05-26 12:38:32 +0100

    delete ImportCompaniesFromList - we have overlapping methods at the moment (create_all_relevant_jobs, create_job_from_url, import_companies_from_list etc.)

--- app/services/scraper/devit_jobs.rb
+++ app/services/scraper/devit_jobs.rb
@@ -19,8 +19,6 @@ module Scraper
       end
 
       p "grabbing jobs with DevIT redirects..."
-      # This can be handled by CreateJobFromUrl
-      ImportCompaniesFromList.new(@redirect_urls).call unless @redirect_urls.empty?
     end
 
     private
