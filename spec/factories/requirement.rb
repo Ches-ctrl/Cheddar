f9439a464f7ba4608f354df62171390f50fcc1af
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-05-26 08:25:43 +0100

    add back requirement factory

---
+++ spec/factories/requirement.rb
@@ -0,0 +1,21 @@
+FactoryBot.define do
+  factory :requirement do
+    job
+    work_eligibility { [true, false].sample }
+    # difficulty { %w[easy medium hard].sample }
+    # no_of_qs { Faker::Number.between(from: 0, to: 30) }
+    # create_account { Faker::Boolean.boolean }
+    # resume { Faker::Boolean.boolean }
+    # cover_letter { Faker::Boolean.boolean }
+    # video_interview { Faker::Boolean.boolean }
+    # online_assessment { Faker::Boolean.boolean }
+    # first_round { Faker::Boolean.boolean }
+    # second_round { Faker::Boolean.boolean }
+    # third_round { Faker::Boolean.boolean }
+    # assessment_centre { Faker::Boolean.boolean }
+
+    trait :with_job do
+      association :job
+    end
+  end
+end
