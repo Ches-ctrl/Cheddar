becf25931e9e72d86d2e0a2e7afa397719afca5e
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-05-26 17:17:07 +0100

    add tag

--- spec/services/net_zero/zero_tracker_spec.rb
+++ spec/services/net_zero/zero_tracker_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe NetZero::ZeroTracker do
+RSpec.describe NetZero::ZeroTracker, zero_tracker: true do
   describe "ZeroTracker" do
     pending "add some examples (or delete) #{__FILE__}"
   end
