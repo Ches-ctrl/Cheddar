612b0e68b3cb3cfdba3218949781d29cda7d47e1
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-05-26 11:14:16 +0100

    add pending label for later

--- spec/features/company_page_spec.rb
+++ spec/features/company_page_spec.rb
@@ -1,14 +1,17 @@
 require 'rails_helper'
 
 RSpec.feature "CompanyPage", type: :feature, company_page: true do
-  let(:company) { create(:company, url_website: "http://example.com", url_linkedin: "http://linkedin.com/in/example", url_careers: nil, url_ats_api: "http://ats.example.com") }
+  pending "add some examples (or delete) #{__FILE__}"
 
-  scenario "User visits the company show page" do
-    visit company_path(company)
+  # let(:company) { create(:company, url_website: "http://example.com", url_linkedin: "http://linkedin.com/in/example", url_careers: nil, url_ats_api: "http://ats.example.com") }
 
-    expect(page).to have_link("Website", href: "http://example.com")
-    expect(page).to have_link("LinkedIn", href: "http://linkedin.com/in/example")
-    expect(page).not_to have_link("Careers Site")
-    expect(page).to have_link("ATS Site", href: "http://ats.example.com")
-  end
+  # scenario "User visits the company show page" do
+
+  #   visit company_path(company)
+
+  #   expect(page).to have_link("Website", href: "http://example.com")
+  #   expect(page).to have_link("LinkedIn", href: "http://linkedin.com/in/example")
+  #   expect(page).not_to have_link("Careers Site")
+  #   expect(page).to have_link("ATS Site", href: "http://ats.example.com")
+  # end
 end
--- spec/features/job_page_spec.rb
+++ spec/features/job_page_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
 RSpec.feature "JobPage", type: :feature, job_page: true do
-  let(:company) { create(:company) }
-  let(:job) { create(:job, company: company) }
+  pending "add some examples (or delete) #{__FILE__}"
 
-  scenario "User visits the job show page" do
-    visit job_path(job)
+  # let(:company) { create(:company) }
+  # let(:job) { create(:job, company: company) }
 
-    expect(page).to have_content(job.title)
-    expect(page).to have_content(company.name)
-  end
+  # scenario "User visits the job show page" do
+  #   visit job_path(job)
+
+  #   expect(page).to have_content(job.title)
+  #   expect(page).to have_content(company.name)
+  # end
 end
