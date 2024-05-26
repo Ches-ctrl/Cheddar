0170c7efe6c29651a1be2e517fa36147920f6bc2
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-05-26 22:33:32 +0100

    adding notes on tests for later

--- spec/services/applier/apply_to_job_spec.rb
+++ spec/services/applier/apply_to_job_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Applier::ApplyToJob do
   describe "ApplyToJob" do
     pending "add some examples (or delete) #{__FILE__}"
   end
+
+  describe "User should not be able to see applied-to jobs on index page" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+
+  describe "User should not be able to apply to jobs twice" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
 end
