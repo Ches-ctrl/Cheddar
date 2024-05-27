9352b7e167f79ffe42f85c07a0f4a0c12c7a39fa
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-05-27 16:25:12 +0100

    add faraday as part of application services to be inherited by others

--- app/services/application_service.rb
+++ app/services/application_service.rb
@@ -1,2 +1,22 @@
-class ApplicationService # rubocop:disable Lint/EmptyClass
+class ApplicationService
+  # TODO: Implement these core helper classes once we've checked for no unintended consequences
+  # def self.call(*args, &block)
+  #   new(*args, &block).call
+  # end
+
+  private
+
+  def fetch_json(url)
+    response = Faraday.get(url)
+    JSON.parse(response.body)
+  rescue Faraday::Error => e
+    Rails.logger.error "HTTP request failed: #{e.message}"
+    nil
+  rescue JSON::ParserError => e
+    Rails.logger.error "Failed to parse JSON: #{e.message}"
+    nil
+  rescue StandardError => e
+    Rails.logger.error "An unexpected error occurred: #{e.message}"
+    nil
+  end
 end
