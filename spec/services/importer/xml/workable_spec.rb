8c071e3d1a343bce97518bd48b995db88997c00f
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-05-27 22:34:54 +0100

    start adding tests for workable - not yet properly setup

--- spec/services/importer/xml/workable_spec.rb
+++ spec/services/importer/xml/workable_spec.rb
@@ -1,5 +1,16 @@
-RSpec.describe Importer::Xml::Workable do
-  describe "Workable XML Integration" do
-    pending "add some examples (or delete) #{__FILE__}"
+require 'rails_helper'
+
+RSpec.describe Importer::Xml::Workable, type: :service, workable: true do
+  describe "#import_xml", vcr: { cassette_name: 'workable_xml' } do
+    let(:workable) { Importer::Xml::Workable.new }
+
+    before do
+      allow(ApplicantTrackingSystem).to receive(:find_by).with(name: 'Workable').and_return(double('ATS', url_xml: 'https://www.workable.com/boards/workable.xml'))
+    end
+
+    it "fetches and parses the XML data" do
+      workable.import_xml
+      expect(workable.job_posting_urls).not_to be_empty
+    end
   end
 end
