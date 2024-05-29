ab35660fbe097f1ece9317e4da3030004cf54750
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-05-29 18:25:58 +0100

    move sort csv logic out of rake file and refactor to be more DRY

--- app/services/csv/sort_csv.rb
+++ app/services/csv/sort_csv.rb
@@ -1,28 +1,54 @@
 module Csv
   class SortCsv
     def sort_ats_identifiers
-      csv_data = CSV.read('storage/csv/ats_identifiers.csv', headers: true)
-      p csv_data.length
+      csv_data = read_csv('storage/csv/ats_identifiers.csv')
+      return unless csv_data
 
-      rows_as_strings = csv_data.map(&:to_s)
-      rows_as_strings.map! { |row| row.gsub("\n", "") }
-      sorted_data = rows_as_strings.sort
+      sorted_data = sort_csv_data(csv_data, %w[ats ats_identifier])
+      write_csv('storage/csv/ats_identifiers.csv', sorted_data, %w[ats ats_identifier])
 
-      CSV.open('storage/csv/ats_identifiers.csv', 'w', write_headers: true, headers: ['ats', 'ats_identifier']) do |csv|
-        sorted_data.each { |row| csv << row.split(',') }
-      end
-
-      puts "CSV file sorted successfully."
+      puts "ATS identifiers CSV file sorted successfully."
     end
 
     def sort_company_urls
-      csv = CSV.read('storage/new/company_urls.csv', headers: true)
-      sorted = csv.sort_by { |row| row['company_url'] }
+      csv_data = read_csv('storage/new/company_urls.csv')
+      return unless csv_data
 
-      CSV.open('storage/new/company_urls.csv', 'w') do |csv|
-        csv << ['company_url']
-        sorted.each { |row| csv << [row['company_url']] }
+      sorted_data = csv_data.sort_by { |row| row['company_url'] }
+      write_csv('storage/new/company_urls.csv', sorted_data, ['company_url'])
+
+      puts "Company URLs CSV file sorted successfully."
+    end
+
+    def sort_job_urls
+      csv_data = read_csv('storage/new/job_posting_urls.csv')
+      return unless csv_data
+
+      sorted_data = csv_data.sort_by { |row| row['posting_url'] }
+      write_csv('storage/new/job_posting_urls.csv', sorted_data, ['posting_url'])
+
+      puts "Job posting URLs CSV file sorted successfully."
+    end
+
+    private
+
+    def read_csv(file_path)
+      CSV.read(file_path, headers: true)
+    rescue StandardError => e
+      puts "Error reading CSV file #{file_path}: #{e.message}"
+      nil
+    end
+
+    def sort_csv_data(csv_data, headers)
+      csv_data.sort_by(&:to_s).map { |row| row.to_h.values_at(*headers) }
+    end
+
+    def write_csv(file_path, data, headers)
+      CSV.open(file_path, 'w', write_headers: true, headers:) do |csv|
+        data.each { |row| csv << row }
       end
+    rescue StandardError => e
+      puts "Error writing to CSV file #{file_path}: #{e.message}"
     end
   end
 end
--- lib/tasks/sort_csv.rake
+++ lib/tasks/sort_csv.rake
@@ -5,18 +5,12 @@ namespace :sort_csv do
   end
 
   desc "Sort CSV - company_urls"
-  task sort_company_url_list: :environment do
+  task sort_company_urls: :environment do
     Csv::SortCsv.new.sort_company_urls
   end
 
-  desc "Sort CSV - job_posting_urls"
-  task sort_job_postings: :environment do
-    csv = CSV.read('storage/new/job_posting_urls.csv', headers: true)
-    sorted = csv.sort_by { |row| row['posting_url'] }
-
-    CSV.open('storage/new/job_posting_urls.csv', 'w') do |csv|
-      csv << ['posting_url']
-      sorted.each { |row| csv << [row['posting_url']] }
-    end
+  desc "Sort CSV - job_urls"
+  task sort_job_urls: :environment do
+    Csv::SortCsv.new.sort_job_urls
   end
 end
