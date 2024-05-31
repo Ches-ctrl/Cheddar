5b9fbbe9906379c2a4907f6503a807ed939c766f
Matt Manes <mattmanes@pm.me>
2024-05-31 14:41:38 -0500

    perf: add crawl and time limits

--- app/services/company_crawler/company_crawler.rb
+++ app/services/company_crawler/company_crawler.rb
@@ -6,6 +6,7 @@ require "pathname"
 # TODO: There's no realtime progress update
 class CompanyCrawler
   attr_reader :ats_hits
+  attr_accessor :max_crawl, :max_time
 
   # Requires a url string
   def initialize(url)
@@ -38,16 +39,50 @@ class CompanyCrawler
     return ats_urls
   end
 
+  # Get current timestamp
+  def current_time
+    return Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  end
+
+  # Return elapsed time
+  def elapsed_time
+    return current_time - @start_time
+  end
+
+  # Return `true` if max crawl exceeded
+  def max_crawl_exceeded?(spider)
+    return false unless !@max_crawl.nil? && @max_crawl < spider.history.length
+
+    return true
+  end
+
+  # Return `true` if max time exceeded
+  def max_time_exceeded?
+    return false unless !@max_time.nil? && @max_time <= elapsed_time
+
+    return true
+  end
+
+  # Return `true` if limits exceeded
+  def limits_exceeded?(spider)
+    return max_crawl_exceeded?(spider) || max_time_exceeded? || !@ats_hits.empty? # TODO: make number of hits to quit variable
+  end
+
   public
 
-  # Start crawling company website for its ats board
-  def crawl
+  # Start crawling company website for its ats board.
+  # `max_crawl`: optional max number of urls to scan before quitting
+  # `max_time`: optional max number of seconds to crawl before quitting
+  def crawl(max_crawl: nil, max_time: nil)
+    @max_crawl = max_crawl
+    @max_time = max_time
+    @start_time = current_time
     puts "Beginning crawl of #{@starting_url}..."
     Spidr.site(@starting_url, strip_fragments: true, strip_query: true, robots: false) do |spider|
       spider.priority_stubs = @careers_page_stubs
       spider.every_page do |page|
-        # quit crawling if we found the board
-        spider.pause! unless @ats_hits.empty? # TODO: make number of hits to quit variable
+        # quit crawling if limits exceed
+        spider.pause! if limits_exceeded?(spider)
         puts "Scanning `#{page.url}`"
         urls = get_urls_from_source(page.body)
         @ats_hits += get_ats_urls(urls)
