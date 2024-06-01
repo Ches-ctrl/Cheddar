b1c004baddf44fde9bdc43aae1f3c73d072b0068
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-06-01 07:36:10 +0100

    company crawler edits

--- lib/constants.rb
+++ lib/constants.rb
@@ -255,4 +255,21 @@ module Constants
     'Structured',
     'Ad-hoc'
   ].freeze
+
+  CAREERS_STUBS = [
+    "jobs",
+    "careers",
+    "get-involved",
+    "positions",
+    "job-openings",
+    "current-job-openings",
+    "careers-list",
+    "open-careers",
+    "open-positions",
+    "opportunities",
+    "open-roles",
+    "current-openings",
+    "hiring",
+    "apply"
+  ].freeze
 end
