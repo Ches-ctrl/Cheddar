126dd01cffcc8a9431fda4d2f370cb834eb57046
patanj101 <patanj101@gmail.com>
2024-06-09 21:25:42 +0200

    Change logo & add switch for dark mode

--- app/javascript/controllers/template/theme_controller.js
+++ app/javascript/controllers/template/theme_controller.js
@@ -1,6 +1,8 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static targets = ["whiteLogo", "blackLogo"];
+
   toggleTheme() {
     const html = document.querySelector("html");
     const currentScheme = html.style.colorScheme;
@@ -8,5 +10,8 @@ export default class extends Controller {
     const newScheme = currentScheme === 'light' ? 'dark' : 'light';
     html.style.colorScheme = newScheme;
     html.classList.toggle("dark");
+    this.blackLogoTarget.classList.toggle("hidden");
+    this.whiteLogoTarget.classList.toggle("hidden");
+
   }
 }
