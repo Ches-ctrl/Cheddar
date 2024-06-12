4c6cf7a70fbe0f80b87f263d1dc173848e8a349e
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-06-12 14:59:57 +0100

    fix issues

--- lib/builders/ats_builder.rb
+++ lib/builders/ats_builder.rb
@@ -2,18 +2,6 @@ module Builders
   class AtsBuilder
     attr_reader :ats_csv
 
-    ATTRIBUTES = {
-      url_identifier: row["url_identifier"],
-      url_website: row["url_website"],
-      url_linkedin: row["url_linkedin"],
-      url_base: row["url_base"],
-      url_api: row["url_api"],
-      url_all_jobs: row["url_all_jobs"],
-      url_xml: row["url_xml"],
-      url_rss: row["url_rss"],
-      login: row["login"]
-    }
-
     def initialize
       @ats_csv = 'storage/csv/ats_systems.csv'
     end
@@ -23,7 +11,17 @@ module Builders
         ats_name = row["ats_name"]
         ats = ApplicantTrackingSystem.find_or_create_by(name: ats_name)
 
-        ats.update(ATTRIBUTES)
+        ats.update(
+          url_identifier: row["url_identifier"],
+          url_website: row["url_website"],
+          url_linkedin: row["url_linkedin"],
+          url_base: row["url_base"],
+          url_api: row["url_api"],
+          url_all_jobs: row["url_all_jobs"],
+          url_xml: row["url_xml"],
+          url_rss: row["url_rss"],
+          login: row["login"]
+        )
 
         if ats
           puts "Created ATS - #{ats.name}"
--- spec/services/csv/export_to_csv_spec.rb
+++ spec/services/csv/export_to_csv_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Csv::ExportDataToCsv do
+RSpec.describe Csv::ExportToCsv do
   describe "CSV Exporting e.g. jobs" do
     pending "add some examples (or delete) #{__FILE__}"
   end
