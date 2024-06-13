e65392c3c46b54b45e36dbb8cb6f606ed5f7cff4
Matt Manes <mattmanes@pm.me>
2024-06-13 16:43:40 -0500

    Initial commit

---
+++ app/services/netzero_crawler/netzero_hit.rb
@@ -0,0 +1,21 @@
+# Represent a possible netzero page hit.
+# The score is based on the number of key term stubs found on the page.
+class NetzeroHit
+  # The url of the page
+  attr_accessor :url
+  # The stubs found on the page
+  attr_accessor :stubs
+
+  # @param url [String]
+  def initialize(url)
+    self.url = url
+    self.stubs = []
+  end
+
+  # The number of stubs for this hit
+  #
+  # @return [Integer]
+  def score
+    return stubs.length
+  end
+end
