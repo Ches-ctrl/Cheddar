3257117ebc7745e1aef5e4fd99db453ca281ff50
Daniel Sussman <daniel.o.sussman@gmail.com>
2024-06-13 16:22:11 +0100

    removed autoloading line and corrected a filepath

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,6 @@ module Cheddar
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
-    config.autoload_paths += %W(#{config.root}/app/services)
 
     # Configuration for the application, engines, and railties goes here.
     #
