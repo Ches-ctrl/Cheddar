2d167bb3f550e999e653207378027ac3f5e74687
patanj101 <patanj101@gmail.com>
2024-06-15 16:53:14 +0200

    Opportunities: Add Link to apply & Fix static text

--- app/helpers/opportunity_helper.rb
+++ app/helpers/opportunity_helper.rb
@@ -21,6 +21,14 @@ module OpportunityHelper
     "#{employment_type} • #{location_type}"
   end
 
+  def opportunity_salary_presentation(opportunity)
+    opportunity.salary.present? ? opportunity.salary : "<br>".html_safe
+  end
+
+  def opportunity_days_remaining(opportunity)
+    opportunity.deadline.nil? || opportunity.deadline < Date.today ? "Rolling" : opportunity.deadline.strftime("%d/%m")
+  end
+
   def seniority_css(opportunity, seniority)
     ' text-gray-300 dark:text-gray-700' unless opportunity.seniority.include?(seniority)
   end
