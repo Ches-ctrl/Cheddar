f22145f09dcfd043fbeb9b195818c7b35a510c8e
patanj101 <patanj101@gmail.com>
2024-06-15 15:22:19 +0200

    Add default value for Chrome Extension Origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch('CHROME_EXTENSION_ORIGIN')
+    origins ENV.fetch('CHROME_EXTENSION_ORIGIN','CHROME_EXTENSION_ORIGIN')
     resource '/api/v0/add_job', headers: :any, methods: [:post]
   end
 end
