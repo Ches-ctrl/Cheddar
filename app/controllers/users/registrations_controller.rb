043ae3dab42edfacc3590947d63029780250b0e6
patanj101 <patanj101@gmail.com>
2024-06-21 00:20:23 +0200

    Lint on Users:RegistrationsController

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
-# module Users
-class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_permitted_parameters
-  # def update
-  #   super
-  #   @user.save!
-  # end
+module Users
+  class RegistrationsController < Devise::RegistrationsController
+    before_action :configure_permitted_parameters
+    # def update
+    #   super
+    #   @user.save!
+    # end
 
-  protected
+    protected
 
-  # def update_resource(resource, params)
-  #   llmmlml
-  # end
-  def after_update_path_for(resource)
-    lmllmml
-    edit_user_registration_path(resource)
+    # def update_resource(resource, params)
+    #   llmmlml
+    # end
+    def after_update_path_for(resource)
+      lmllmml
+      edit_user_registration_path(resource)
+    end
   end
 end
-# end
