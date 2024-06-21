de9262669989a5a7a56a7151872d0473cd61044c
patanj101 <patanj101@gmail.com>
2024-06-22 00:20:55 +0200

    Fixes rspec related to application process new models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,9 @@
 RSpec.describe User do
   describe 'Associations' do
-    it { is_expected.to have_many(:job_applications).dependent(:destroy) }
+    it { is_expected.to have_many(:application_processes).dependent(:destroy) }
+    it { is_expected.to have_many(:job_applications).through(:application_processes) }
     it { is_expected.to have_many(:saved_jobs).dependent(:destroy) }
     it { is_expected.to have_many(:educations).dependent(:destroy) }
-    it { is_expected.to have_many(:jobs).through(:job_applications) }
     it { is_expected.to have_one_attached(:photo) }
     it { is_expected.to have_one_attached(:resume) }
   end
