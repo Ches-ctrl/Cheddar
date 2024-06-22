c1d2abca2d5141c2604baa78914806cc115311b8
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-06-22 11:19:35 +0100

    I think the services are breaking company creation?

--- app/models/company.rb
+++ app/models/company.rb
@@ -27,6 +27,7 @@ class Company < ApplicationRecord
 
   # == Instance Methods =====================================================
 
+  # TODO: Refactor this into a service class as shouldn't sit in the model
   def create_all_relevant_jobs
     jobs_found_or_created = []
     ats = applicant_tracking_system
@@ -64,37 +65,40 @@ class Company < ApplicationRecord
 
   # TODO: Have identified these as the primary drivers of slow test speed due to API calls. Need to decide on next steps.
   # TODO: This is a very hacky temporary solution to speed up tests. Need to fix this
+  # TODO: Remove these as depenedencies - we will find another way to do this
 
-  def set_website_url
-    return if url_website.present?
+  # def set_website_url
+  #   return if url_website.present?
 
-    if Rails.env.production?
-      clearbit_company_info = Categorizer::CompanyDomainService.lookup_domain(name)
-      self.url_website = clearbit_company_info['domain'] if clearbit_company_info && clearbit_company_info['domain'].present?
-    else
-      self.url_website = "https://www.example.com"
-    end
-  end
+  #   if Rails.env.production?
+  #     clearbit_company_info = Categorizer::CompanyDomainService.lookup_domain(name)
+  #     self.url_website = clearbit_company_info['domain'] if clearbit_company_info && clearbit_company_info['domain'].present?
+  #   else
+  #     self.url_website = "https://www.example.com"
+  #   end
+  # end
 
-  def fetch_description
-    if Rails.env.production?
-      inferred_description, @name_keywords = Categorizer::CompanyDescriptionService.lookup_company(name, ats_identifier)
-      self.description = inferred_description if description.blank?
-    else
-      self.description = "A financial services company."
-    end
-  end
+  # def fetch_description
+  #   return if self.description.present?
 
-  def fetch_industry
-    return unless industry == 'n/a'
+  #   if Rails.env.production?
+  #     inferred_description, @name_keywords = Categorizer::CompanyDescriptionService.lookup_company(name, ats_identifier)
+  #     self.description = inferred_description if description.blank?
+  #   else
+  #     self.description = "A financial services company."
+  #   end
+  # end
 
-    if Rails.env.production?
-      industry, subcategory = Categorizer::CompanyIndustryService.lookup_industry(name, @name_keywords)
-      self.industry = industry
-      self.sub_industry = subcategory
-    else
-      self.industry = "Finance"
-      self.sub_industry = "Banking"
-    end
-  end
+  # def fetch_industry
+  #   return unless industry == 'n/a'
+
+  #   if Rails.env.production?
+  #     industry, subcategory = Categorizer::CompanyIndustryService.lookup_industry(name, @name_keywords)
+  #     self.industry = industry
+  #     self.sub_industry = subcategory
+  #   else
+  #     self.industry = "Finance"
+  #     self.sub_industry = "Banking"
+  #   end
+  # end
 end
