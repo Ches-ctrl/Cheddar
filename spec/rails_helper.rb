aa5e8d73cf1ebae44748feba1941258dcc27a74c
Daniel Sussman <daniel.o.sussman@gmail.com>
2024-06-24 11:18:06 +0100

    made cassette filepath relative

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,7 +40,7 @@ end
 
 # VCR Config
 VCR.configure do |config|
-  config.cassette_library_dir = "spec/fixtures/cassettes"
+  config.cassette_library_dir = Rails.root.join('spec', 'fixtures', 'cassettes')
   config.default_cassette_options = { :record => :once }
   config.hook_into :webmock
   config.configure_rspec_metadata!
