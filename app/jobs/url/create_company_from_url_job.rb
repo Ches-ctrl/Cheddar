0f355f0653640f03e146a4dd5d0f66500710a16f
Daniel Sussman <daniel.o.sussman@gmail.com>
2024-06-24 15:30:55 +0100

    fixed filepath

--- app/jobs/url/create_company_from_url_job.rb
+++ app/jobs/url/create_company_from_url_job.rb
@@ -1,12 +1,10 @@
-module Importer
-  module Url
-    class CreateCompanyFromUrlJob < ApplicationJob
-      queue_as :default
-      retry_on StandardError, attempts: 0
+module Url
+  class CreateCompanyFromUrlJob < ApplicationJob
+    queue_as :default
+    retry_on StandardError, attempts: 0
 
-      def perform(url)
-        Importer::URL::CreateCompanyFromUrl.new(url).create_company
-      end
+    def perform(url)
+      Importer::URL::CreateCompanyFromUrl.new(url).create_company
     end
   end
 end
