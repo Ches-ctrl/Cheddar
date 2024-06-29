9860a4a8d348641202577939fedd7371ae76843d
patanj101 <patanj101@gmail.com>
2024-06-29 13:26:52 +0200

    Add form value to application criteria for Capybara

--- app/helpers/payload_application_processes_helper.rb
+++ app/helpers/payload_application_processes_helper.rb
@@ -1,11 +1,18 @@
 module PayloadApplicationProcessesHelper
   def transformed_data(job_application)
-    translator = ApiFormatTranslators::BaseAtsTranslator.create_translator(job_application)
-    if translator
-      translator.translate
-    else
-      "#{job_application.job.applicant_tracking_system.name} translator not found!"
+    # translator = ApiFormatTranslators::BaseAtsTranslator.create_translator(job_application)
+    # if translator
+    #   translator.translate
+    # else
+    #   "#{job_application.job.applicant_tracking_system.name} translator not found!"
+    # end
+
+    transformed_criteria = {}
+    job_application.job.application_criteria.each do |attribute, criteria_hash|
+      value = job_application.additional_info.fetch(criteria_hash['locators'], 'not_found')
+      transformed_criteria[attribute] = criteria_hash.merge(value:)
     end
+    transformed_criteria.to_json
   end
 
   def payload_title(job_application)
