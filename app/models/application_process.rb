d631e3d9022daa00153b6c2332a12a75da2372bf
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-07-01 16:33:13 +0100

    update comments for model

--- app/models/application_process.rb
+++ app/models/application_process.rb
@@ -3,12 +3,11 @@
 class ApplicationProcess < ApplicationRecord
   # == Attributes ===========================================================
   # == Callbacks ============================================================
-  # == Class Methods ========================================================
   # == Constants ============================================================
   # == Extensions ===========================================================
   # serialize :frequent_asked_info, coder: HashSerializer
   # store_accessor :attr1, :attr2, :attr3, :attr4, :attr5
-  # == Instance Methods =====================================================
+
   # == Relationships ========================================================
   belongs_to :user
   has_many :job_applications, dependent: :destroy
@@ -17,6 +16,7 @@ class ApplicationProcess < ApplicationRecord
   # == Validations ==========================================================
   validates :status, presence: true
 
+  # == Instance Methods =====================================================
   def find_application_for_job(job)
     job_applications.find_by(job:)
   end
@@ -24,4 +24,5 @@ class ApplicationProcess < ApplicationRecord
   def submitted?
     submitted_at.present?
   end
+  # == Class Methods ========================================================
 end
