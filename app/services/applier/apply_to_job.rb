f0fa93bc562eec7f29107d711febc29fd0242fc6
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-07-02 15:08:39 +0100

    update ApplyToJob as generic class that will call either FormFiller or ApiApply

--- app/services/applier/apply_to_job.rb
+++ app/services/applier/apply_to_job.rb
@@ -1,44 +1,49 @@
 module Applier
-  # Core class for applying to jobs
-  class ApplyToJob
-    attr_accessor :job_application_id, :user_id
-
-    def initialize(job_application_id, user_id)
-      @job_application_id = job_application_id
-      @user_id = user_id
+  # Core class for applying to jobs using either ApiApply or FormFiller depending on the ATS
+  class ApplyToJob < ApplicationTask
+    def initialize(job_application, payload)
+      @job_application = job_application
+      @job = @job_application.job
+      @payload = payload
     end
 
-    def apply
-      p "Hello from the job application job!"
-      application = JobApplication.find(@job_application_id)
-      job = application.job
-      user = User.find(@user_id)
+    def call
+      return unless processable
 
-      application_criteria = assign_values_to_form(application, user)
-      fields_to_fill = application_criteria
-
-      ## Already done by Payload up to here
-
-      form_filler = Applier::FormFiller.new(job.posting_url, fields_to_fill, @job_application_id)
-      result = form_filler.fill_out_form
-
-      user_channel_name = "job_applications_#{user.id}"
-
-      p "result: #{result}"
-      status = result ? "Applied" : "Submission failed"
-      p "status: #{status}"
-
-      # TODO: Replace this with sidekiq-status gem capabilities
-      ActionCable.server.broadcast(
-        user_channel_name,
-        {
-          event: "job_application_submitted",
-          job_application_id: application.id,
-          user_id: application.user.id,
-          job_id: job.id,
-          status:
-        }
-      )
+      process
+    rescue StandardError => e
+      Rails.logger.error "Error submitting job application: #{e.message}"
+      nil
     end
+
+    private
+
+    def processable
+      @job_application && @job && @payload # && @user
+    end
+
+    def process
+      p "Hello from the job application service class!"
+      p @payload
+      spin_up_form_filler
+    end
+
+    # TODO: In future will route to either FormFiller or ApiApply depending on the ATS - add routing logic
+    def spin_up_form_filler
+      Applier::FormFiller.call(@job.posting_url, @payload, @job_application)
+    end
+
+    # def create_action_cable_broadcast
+    #  ActionCable.server.broadcast(
+    #    user_channel_name,
+    #    {
+    #      event: "job_application_submitted",
+    #      job_application_id: application.id,
+    #      user_id: application.user.id,
+    #      job_id: job.id,
+    #      status:
+    #    }
+    #  )
+    # end
   end
 end
