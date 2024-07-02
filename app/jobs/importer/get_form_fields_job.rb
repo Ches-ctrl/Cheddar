8e9307f0aec2764000df4fd7fb74132ab71b59e8
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-07-02 16:26:51 +0100

    update call to service class and add status

--- app/jobs/importer/get_form_fields_job.rb
+++ app/jobs/importer/get_form_fields_job.rb
@@ -1,10 +1,12 @@
 module Importer
   class GetFormFieldsJob < ApplicationJob
+    include Sidekiq::Status::Worker
+
     queue_as :importers
     sidekiq_options retry: false
 
     def perform(job)
-      Importer::GetFormFields.new(job).perform
+      Importer::GetFormFields.call(job)
     end
   end
 end
