edf4aedd775d149229cbba6d89bd75aea92ca9be
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-07-04 18:50:21 +0100

    turns out getting the assistants API to work is a little bit of a larger job than anticipated

--- app/services/openai.rb
+++ app/services/openai.rb
@@ -1,6 +1,17 @@
 # app/services/openai.rb
 # Class for interacting with OpenAI, primarily for resume analysis during user onboarding in the first instance
 # NB. In progress and not fully functional yet
+# How this works:
+# 1. Initialize the OpenAI client and assistant
+# 2. Create a thread
+# 3. Upload the resume file
+# 4. Add the file to the thread
+# 5. Run the thread through the assistant
+# 6. Return the response & save to cloudinary
+# 7. Update the user_details with the response
+# NB. Note that threads are independent of assistants and thus must be created on @client rather than @assistant
+# NB. May need to use vector stores?
+# HelloGPT - asst_eX0JDDQHnRtmOOgWdB0rNuKY
 class Openai < ApplicationTask
   def initialize
     p "Initialize"
@@ -21,109 +32,110 @@ class Openai < ApplicationTask
   private
 
   def processable
-    @client
+    @client && @assistant && @file_id
   end
 
   def process
     puts "Processing"
+    # file = upload_file
+    # file = retrieve_file
+
     thread_id = create_thread
-    puts thread_id
-    message = create_message(thread_id)
-    puts message
-    list_messages(thread_id)
+    create_message(thread_id)
+
+    run_id, thread_id = create_thread_and_run
+    sleep 1
+
+    # run_id = run_thread(thread_id)
+    poll_response_status(run_id, thread_id)
+
+    messages = @client.messages.list(thread_id:, parameters: { order: 'asc' })
+
+    puts pretty_generate(messages)
+
+    # delete_thread(thread_id)
+    # delete_file(file)
   end
 
-  def admin
-    # Not called but kept for reference regarding the API structure
-    upload_resume
-    list_assistants
-    list_uploaded_files
-    basic_chat
-  end
-
-  def list_assistants
-    response = @client.assistants.list
-    puts pretty_generate(response)
-  end
-
-  def upload_resume
+  def upload_file
     puts "Uploading resume"
     my_file = File.open("public/Obretetskiy_cv.pdf", "rb")
     @client.files.upload(parameters: { file: my_file, purpose: "assistants" })
   end
 
-  def list_uploaded_files
-    puts "Checking resume uploaded"
-    @client.files.list
-  end
-
-  def find_file_by_filename(list, filename)
-    file = list["data"].find { |file| file["filename"] == filename }
-    file["id"]
-  end
-
-  def send_resume_to_openai(_file_id)
-    puts "Processing resume with OpenAI"
-    response = @assistant.chat(
-      parameters: {
-        model: "gpt-4o",
-        # file_id:,
-        attachments: [
-          {
-            file_id: file.id,
-            tools: [{ type: "file_search" }]
-          }
-        ],
-        messages: [{ role: "user", content: "Here is my resume. Tell me my name" }],
-        # messages: [{ role: "user", content: "Respond with a sample JSON"}],
-        temperature: 0.7
-      }
-    )
-    puts response
-    puts pretty_generate(response.dig("choices", 0, "message", "content"))
+  # Used in testing to prevent the need to upload the file each time
+  def retrieve_file
+    puts "Retrieving file"
+    @client.files.retrieve(id: @file_id)
   end
 
   def create_thread
-    puts "Creating thread"
-    thread_response = @assistant.threads.create
-    thread_response['id']
-  end
-
-  def list_messages(thread_id)
-    puts "Listing messages"
-    messages_response = @assistant.threads.messages.list(thread_id)
-    puts messages_response
+    puts "Adding file to thread"
+    thread_response = @client.threads.create
+    thread_response["id"]
   end
 
   def create_message(thread_id)
     puts "Creating message"
-    message_response = @assistant.threads.messages.create(
-      thread_id,
-      {
-        role: 'user',
-        content: 'My name is charlie. What is my name?'
-        # attachments: [
-        #   {
-        #     file_id: file_id,
-        #     tools: [{ type: 'file_search' }]
-        #   }
-        # ]
+    message = @client.messages.create(
+      thread_id:,
+      parameters: {
+        role: "user",
+        content: "The name on my CV is Charlie. What is my name?"
       }
     )
-    puts message_response
+    puts pretty_generate(message)
   rescue StandardError => e
-    puts e
+    puts "An error occurred: #{e.message}"
   end
 
-  def basic_chat
-    response = @client.chat(
-      parameters: {
-        model: "gpt-4o",
-        messages: [{ role: "user", content: "Hello!" }],
-        temperature: 0.7
-      }
-    )
-    puts response.dig("choices", 0, "message", "content")
+  def run_thread(thread_id)
+    puts "Running thread"
+    run = @client.runs.create(thread_id:,
+                              parameters: {
+                                assistant_id: "asst_62b4xuE3RQvqlIDT1AYed0wZ",
+                                max_prompt_tokens: 256,
+                                max_completion_tokens: 16
+                              })
+    run["id"]
+  rescue StandardError => e
+    puts "An error occurred: #{e.message}"
+  end
+
+  def poll_response_status(run_id, thread_id)
+    puts "Polling response status"
+    loop do
+      response = @client.runs.retrieve(id: run_id, thread_id:)
+      status = response['status']
+
+      case status
+      when 'queued', 'in_progress', 'cancelling'
+        puts 'Sleeping'
+        sleep 3
+      when 'completed'
+        puts 'Completed'
+        break
+      when 'requires_action'
+        puts 'Requires action'
+      when 'cancelled', 'failed', 'expired'
+        puts "Run failed"
+        puts response
+        puts response['last_error'].inspect
+        break
+      else
+        puts "Unknown status response: #{status}"
+      end
+    end
+  rescue StandardError => e
+    puts "An error occurred: #{e.message}"
+  end
+
+  def create_thread_and_run
+    puts "Creating thread and running"
+    response = @client.runs.create_thread_and_run(parameters: { assistant_id: "asst_eX0JDDQHnRtmOOgWdB0rNuKY" })
+    run_id = response['id']
+    thread_id = response['thread_id']
+    [run_id, thread_id]
   end
 
   def pretty_generate(response)
