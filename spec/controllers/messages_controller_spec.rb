f51d58855a135633796929a866de78fe097af1e5
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-07-04 07:13:32 +0100

    update spec file

--- spec/controllers/messages_controller_spec.rb
+++
@@ -1,2 +0,0 @@
-RSpec.describe MessagesController do
-end
---
+++ spec/controllers/resume_parser_controller_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe ResumeParserController do
+  describe "Resume Controller Spec" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+end
