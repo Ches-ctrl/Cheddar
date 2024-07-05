2510133e90025bc5007a055a0256b5d7b0e0e1bd
Matt Manes <mattmanes@pm.me>
2024-07-05 15:47:57 -0500

    tests: add bamboohr crawl test

--- spec/crawlers/company_crawler_spec.rb
+++ spec/crawlers/company_crawler_spec.rb
@@ -12,5 +12,16 @@ RSpec.describe Crawlers::CompanyCrawler do
             expect(hits.length).to eql(1)
             expect(hits[0]).to eql("https://boards.greenhouse.io/strava")
         end
+        
+        it "returns https://globalenergymonitor.bamboohr.com/careers" do
+            crawler = Crawlers::CompanyCrawler.new('https://globalenergymonitor.org')
+            max_crawl = 10
+            max_time = 10
+            max_hits = 1
+            crawler.set_limits(max_crawl, max_time, max_hits)
+            hits = crawler.crawl
+            expect(hits.length).to eql(1)
+            expect(hits[0]).to eql('https://globalenergymonitor.bamboohr.com/careers')
+        end
     end
 end
