35f6852bb28791d39708b2dceb0243546df4574f
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-07-06 21:38:23 +0100

    getting there

--- app/services/importer/get_form_fields.rb
+++ app/services/importer/get_form_fields.rb
@@ -7,6 +7,13 @@ module Importer
   # Splits based on category of fields - main, custom, demographic, eeoc
   # In a standard greenhouse form, every field exists within a field tag
   # Notes - exclude dev-fields-1, hidden fields
+  # Instructions:
+  # 1. Parse the HTML of the job posting
+  # 2. Parse the form from the HTML (check for application form)
+  # 3. Get the labels
+  # 3. Get the form elements (input, textarea, select), group according to their label
+  # 5. Get the other characteristics for each form element
+  # NB. Exclude non-relevant fields
   # TODO: Separate parsing for job-boards vs boards (embedded / customised forms)
   # TODO: Add routing logic - in future will route to either NokoFields / CapyFields or ApiFields depending on the ATS
   class GetFormFields < ApplicationTask
@@ -14,12 +21,14 @@ module Importer
       # @job = job
       # @url = @job.posting_url
       # @url = "https://job-boards.greenhouse.io/monzo/jobs/6076740"
-      # @url = "https://boards.greenhouse.io/cleoai/jobs/4628944002"
-      @url = "https://boards.greenhouse.io/axios/jobs/6009256#app"
+      @url = "https://boards.greenhouse.io/cleoai/jobs/4628944002"
+      # @url = "https://boards.greenhouse.io/axios/jobs/6009256#app"
       # @url = "https://boards.greenhouse.io/11fs/jobs/4060453101"
       # @url = "https://boards.greenhouse.io/forter/jobs/7259821002"
       # @url = "https://stripe.com/jobs/listing/account-executive-digital-natives/5414838/apply"
+      @ats_sections = %w[main_fields custom_fields demographic_questions eeoc_fields]
       @fields = {}
+      @errors = false
     end
 
     def call
@@ -37,25 +46,25 @@ module Importer
       @url # && @job
     end
 
-    # Instructions:
-    # 1. Parse the HTML of the job posting
-    # 2. Parse the form from the HTML (check for application form)
-    # 3. Get the form elements (input, textarea, select)
-    # 4. Get the label for each form element
-    # 5. Get the other characteristics for each form element
-    # NB. Exclude non-relevant fields
-
+    # HTML > Form > Section > Field > Label > Input > Options
     def process
       p "Hello from GetFormFields!"
       doc = parse_html
       form = find_form(doc)
-      return @fields unless form # Add error handling
+      return @fields unless form
 
-      # This can be refactored to use a loop, with an ATS having a list of field types as a characteristic that is passed to GetFormFields
-      process_fields(form, "main_fields", "Main fields")
-      process_fields(form, "custom_fields", "Custom fields")
-      process_fields(form, "demographic_questions", "Demographic fields")
-      process_fields(form, "eeoc_fields", "EEOC fields")
+      @ats_sections.each do |section_name|
+        section_html = split_by_section(form, section_name)
+        next unless section_present?(section_html, section_name.humanize)
+
+        fields = split_by_field(section_html)
+        results = fields.map { |field| process_field(field) }
+
+        puts pretty_generate(results)
+
+
+        # @fields[section_name] = { questions: process_fields(section_html) }
+      end
 
       puts pretty_generate(@fields)
       @fields
@@ -66,7 +75,6 @@ module Importer
       Nokogiri::HTML(html)
     end
 
-    # Checks for forms with specific IDs, then for specific keywords, then any form
     def find_form(doc)
       form = doc.css('form#application-form, form#application_form').first
       return form if form
@@ -80,41 +88,78 @@ module Importer
       "Form not found: #{e.message}"
     end
 
-    def process_fields(form, css_selector, field_name)
-      puts "Processing fields"
-      partial_form = form.css("##{css_selector}")
-      return unless fields_present?(partial_form, field_name)
-
-      @fields[css_selector] = { questions: get_form_elements(partial_form) }
+    def split_by_section(form, css_selector)
+      form.css("##{css_selector}")
     end
 
-    def fields_present?(fields, fields_name)
+    def section_present?(fields, fields_name)
       fields.empty? ? puts("#{fields_name} - NO") : puts("#{fields_name} - YES")
       !fields.empty?
     end
 
-    def get_form_elements(partial_form)
-      puts "Getting form elements"
+    def split_by_field(form)
+      form.css('.field')
+    end
+
+    def process_fields(fields)
+      fields.map do |field|
+        label = field.at('label')&.text&.strip
+        inputs = field.css('input, select, textarea').map do |input|
+          {
+            name: input['name'],
+            type: input['type'],
+            value: input['value'],
+            id: input['id'],
+            checked: input['checked'] == 'checked'
+          }
+        end
+        { label:, inputs: }
+      end
+    end
+
+    def process_field(field)
+      label = field.at('label')&.text&.strip
+      options = field.css('input, select, textarea').map do |input|
+        next if input['type'] == 'hidden' || input['id'].nil?
+
+        {
+          id: input['id'],
+          label: get_label(field, input),
+          required: input['aria-required'],
+          type: input['type'] || input.name,
+          value: input['value'],
+          set: input['set'],
+        }
+      end
+      { label:, options: }
+    end
+
+    # ==================
+
+    def extract_questions(section_html)
       # TODO: Handle textarea, select & other input types
       local_fields = []
 
-      # Can get additional fields e.g. name, autocomplete but these have little informational value from the looks of things
-      partial_form.css('input, textarea, select').each do |input|
-        next if input['type'] == 'hidden'
+      section_html.css('input, textarea, select').each do |input|
+        next if input['type'] == 'hidden' || input['type'] == 'checkbox' || input['type'] == 'radio'
 
         id = input['id']
         next unless id
 
-        label = get_label(partial_form, input)
+        label = get_label(section_html, input)
         required = input['aria-required']
         max_length = input['maxlength']
         type = input['type'] || input.name
+        value = input['value']
+        set = input['set']
 
         field = {
           id:,
           label:,
           required:,
           type:,
+          value:,
+          set:,
           max_length:
         }
 
@@ -125,18 +170,12 @@ module Importer
 
     def get_label(form, input)
       # First find a for= element, then look for label text, then return the text of the parent label element if the label is a parent of the input
-      label = form.css("label[for='#{input['id']}']").text
-      label.empty? ? input.parent.css('label').text.strip : label
-      label.gsub(/\s+/, ' ').strip
-      # label.empty? ? input.parent.children.select(&:text?).map(&:text).join.strip : label # Alternative method for getting labels
-      # label.empty? ? input.parent.text.strip : label # Old method for getting label text
-    end
+      label = form.css("label[for='#{input['id']}']").text # best way for getting labels
+      label.empty? ? input.parent.css('label').text.strip : label # gets labels from parent element
 
-    def collect_radio_options(doc, input)
-      name = input['name']
-      doc.css("input[name='#{name}'][type='radio']").map do |radio|
-        { "label" => get_label(doc, radio), "value" => radio['value'] }
-      end
+      label.empty? ? input.parent.children.select(&:text?).map(&:text).join.strip : label # Alternative method for getting labels
+      label.gsub(/\s+/, ' ').strip # Works for Axios with above method
+      label.empty? ? input.parent.text.strip : label # Old method for getting label text (works for Cleo.ai)
     end
 
     # May need to remove trailing asterisk, underscore or space
@@ -157,6 +196,21 @@ module Importer
     end
   end
 end
+
+# if type == 'checkbox'
+#   checkboxes[label] ||= { id:, label:, required:, type: 'checkbox', options: [] }
+#   option_label = input.parent.parent.parent.css('label').text.strip
+#   checkboxes[label][:options] << { text: option_label, value: input['value'] }
+# else
+#   field = {
+#     id:,
+#     label:,
+#     required:,
+#     type:,
+#     max_length:
+#   }
+#   local_fields << field
+# end
 
 # def scrape_form(url)
 #   form.css('input, textarea, select').each do |input|
@@ -188,22 +242,14 @@ end
 #   end
 # end
 
-# def process_main_fields(form)
-#   main_fields = form.css("#main_fields")
-#   fields_present?(main_fields, "Main fields")
+# def split_by_label(question)
+#   label = question.children.select(&:text?).map(&:text).join.strip
+#   puts label
 # end
 
-# def process_custom_fields(form)
-#   custom_fields = form.css("#custom_fields")
-#   fields_present?(custom_fields, "Custom fields")
-# end
-
-# def process_demographic_fields(form)
-#   demographic_fields = form.css("#demographic_questions")
-#   fields_present?(demographic_fields, "Demographic fields")
-# end
-
-# def process_eeoc_fields(form)
-#   eeoc_fields = form.css("#eeoc_fields")
-#   fields_present?(eeoc_fields, "EEOC fields")
+# def collect_radio_options(doc, input)
+#   name = input['name']
+#   doc.css("input[name='#{name}'][type='radio']").map do |radio|
+#     { "label" => get_label(doc, radio), "value" => radio['value'] }
+#   end
 # end
