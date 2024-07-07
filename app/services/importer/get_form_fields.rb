4d7c12195b6443939b79dd4c1f9243b59a8b9535
Charlie Cheesman <djnr.cheesman@gmail.com>
2024-07-07 13:09:19 +0100

    handle additional field sets

--- app/services/importer/get_form_fields.rb
+++ app/services/importer/get_form_fields.rb
@@ -11,6 +11,7 @@ module Importer
   # 3. Get the inputs relating to the question (input, textarea, select)
   # 5. Get the values for each input
   # Allowable file types (Greenhouse): (File types: pdf, doc, docx, txt, rtf)
+  # NB. This will not pull all the fields if there is an additional section not listed in @ats_sections (?rewrite?)
   # TODO: Handle security codes (verification fields)
   class GetFormFields < ApplicationTask
     def initialize
@@ -18,11 +19,12 @@ module Importer
       # @url = @job.posting_url
       # @url = "https://job-boards.greenhouse.io/monzo/jobs/6076740"
       # @url = "https://boards.greenhouse.io/cleoai/jobs/4628944002"
+      @url = "https://boards.greenhouse.io/cleoai/jobs/7301308002"
       # @url = "https://boards.greenhouse.io/axios/jobs/6009256#app"
       # @url = "https://boards.greenhouse.io/11fs/jobs/4060453101"
-      @url = "https://boards.greenhouse.io/forter/jobs/7259821002"
+      # @url = "https://boards.greenhouse.io/forter/jobs/7259821002"
       # @url = "https://stripe.com/jobs/listing/account-executive-digital-natives/5414838/apply"
-      @ats_sections = %w[main_fields custom_fields demographic_questions eeoc_fields]
+      @ats_sections = %w[main_fields custom_fields demographic_questions eeoc_fields data_compliance security_code_fields]
       @fields = {}
       @errors = false
     end
@@ -109,7 +111,38 @@ module Importer
       end
     end
 
+    def process_checkbox_radio(label, field)
+      id = field.at('input[type="hidden"][id*="question_id"]')['id']
+      options = field.css('input[type="checkbox"], input[type="radio"]').map do |input|
+        next if input['type'] == 'hidden'
+
+        {
+          option: get_label(field, input),
+          class: input['class'],
+          id: input['id'] # For uniquely identifying the option
+          # value: input['value'], # Useful for submitting post requests
+        }
+      end
+      { label:, id:, type: 'checkbox', options: }
+    end
+
+    def process_select(label, field)
+      id = field.at('input[type="hidden"][id*="question_id"]')['id']
+      options = field.css('option').map do |option|
+        next if option.text.strip == "--" || option.text.strip == "Please select"
+
+        {
+          option: option.text.strip,
+          class: option['class'],
+          id: option['id'] # For uniquely identifying the option
+          # value: option['value'], # Useful for submitting post requests
+        }
+      end
+      { label:, id:, type: 'select', options: }
+    end
+
     def process_input(_label, field)
+      # ID set to answer value at the moment but could instead by the question_id (simple gsub also works)
       field.css('input, textarea').map do |input|
         next if input['type'] == 'hidden' || input['id'].nil? || input['id'].include?('dev-field-1')
 
@@ -119,47 +152,24 @@ module Importer
           type: input['type'] || input.name,
           max_length: input['maxlength'],
           id: input['id']
-          # set: input['set'],
           # value: input['value'], # Useful for submitting post requests
         }
       end
     end
 
-    def process_select(label, field)
-      options = field.css('option').map do |option|
-        next if option.text.strip == "--" || option.text.strip == "Please select"
-
-        {
-          option: option.text.strip,
-          class: option['class']
-          # id: option['id'], # For uniquely identifying the option
-          # value: option['value'], # Useful for submitting post requests
-        }
-      end
-      { label:, type: 'select', options: }
-    end
-
-    def process_checkbox_radio(label, field)
-      options = field.css('input[type="checkbox"], input[type="radio"]').map do |input|
-        next if input['type'] == 'hidden'
-
-        {
-          option: get_label(field, input),
-          class: input['class']
-          # id: input['id'], # For uniquely identifying the option
-          # value: input['value'], # Useful for submitting post requests
-        }
-      end
-      { label:, type: 'checkbox', options: }
-    end
-
-    # First find a for= element, then look for label text
+    # First find a for= element, then look for label text - not fully working yet
     def get_label(form, input)
       label = form.css("label[for='#{input['id']}']").text # best way for getting labels
-      label.empty? ? input.parent.css('label').text.strip : label # gets labels from parent element
-      label.empty? ? input.parent.children.select(&:text?).map(&:text).join.strip : label # Alternative method for getting labels
+      # puts "0. #{label}"
+      label.empty? ? label = input.parent.css('label').text.strip : label # gets labels from parent element
+      # puts "1. #{label}"
+      label.empty? ? label = input.parent.children.select(&:text?).map(&:text).join.strip : label # Alternative method for getting labels
+      # puts "2. #{label}"
       label.gsub(/\s+/, ' ').strip # (Works for Axios with above method)
-      label.empty? ? input.parent.text.strip : label # (Works for Cleo.ai)
+      # puts "3. #{label}"
+      label.empty? ? label = input.parent.text.strip : label # (Works for Cleo.ai)
+      # puts "4. #{label}"
+      label
     end
 
     # May need to remove trailing asterisk, underscore or space
