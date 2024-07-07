1e8ed1ac5269a83c222c1456fe6807b7f5ab149a
patanj101 <patanj101@gmail.com>
2024-07-07 19:29:25 +0200

    Fix Rubocop offenses

--- app/controllers/avo/saved_searches_controller.rb
+++ app/controllers/avo/saved_searches_controller.rb
@@ -1,4 +1,6 @@
 # This controller has been generated to enable Rails' resource routes.
 # More information on https://docs.avohq.io/3.0/controllers.html
-class Avo::SavedSearchesController < Avo::ResourcesController
+module Avo
+  class SavedSearchesController < Avo::ResourcesController
+  end
 end
