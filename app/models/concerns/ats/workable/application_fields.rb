620d35a3034186348f902a88679e6e52087a0f9d
patanj101 <patanj101@gmail.com>
2024-07-16 10:03:36 +0200

    ApplicationFields: update

--- app/models/concerns/ats/workable/application_fields.rb
+++ app/models/concerns/ats/workable/application_fields.rb
@@ -8,13 +8,11 @@ module Ats
       def get_application_criteria(job, _data)
         # TODO: get application_criteria from API
         p "Getting Workable application criteria"
-        # job.application_criteria = PERSONAL_FIELDS.merge(PROFILE_FIELDS).merge(DETAILS_FIELDS).merge(ADDITIONAL_FIELDS)
-        # job.save
         job.build_application_criterion(form_structure: {
-          **PERSONAL_FIELDS,
-          **PROFILE_FIELDS,
-          **DETAILS_FIELDS,
-          **ADDITIONAL_FIELDS
+          **NEW_CORE_FIELDS # ,
+                                          # **PROFILE_FIELDS,
+                                          # **DETAILS_FIELDS,
+                                          # **ADDITIONAL_FIELDS
                                         })
         job.save
       end
@@ -24,6 +22,151 @@ module Ats
       end
 
       # Organised into sections (sections have labels)
+      NEW_CORE_FIELDS = [
+        {
+          data_source: "scraping",
+          section_slug: "core_fields",
+          title: "Main critera",
+          description: nil,
+          questions: [
+            {
+              attribute: "first_name",
+              required: true,
+              label: "First Name",
+              description: nil,
+              fields: [
+                {
+                  id: nil,
+                  selector: "firstname",
+                  type: 'input_text',
+                  max_length: 50,
+                  options: []
+                }
+              ]
+            },
+            {
+              attribute: "last_name",
+              required: true,
+              label: "Last Name",
+              description: nil,
+              fields: [
+                {
+                  id: nil,
+                  selector: "lastname",
+                  type: 'input_text',
+                  max_length: 50,
+                  options: []
+                }
+              ]
+            },
+            {
+              attribute: "email",
+              required: true,
+              label: "Email",
+              description: nil,
+              fields: [
+                {
+                  id: nil,
+                  selector: "email",
+                  type: 'input_text',
+                  max_length: 50,
+                  options: []
+                }
+              ]
+            },
+            {
+              attribute: "headline",
+              required: true,
+              label: "Headline",
+              description: nil,
+              fields: [
+                {
+                  id: nil,
+                  selector: "headline",
+                  type: 'input_text',
+                  max_length: 50,
+                  options: []
+                }
+              ]
+            },
+            {
+              attribute: "phone_number",
+              required: true,
+              label: "Phone Number",
+              description: nil,
+              fields: [
+                {
+                  id: nil,
+                  selector: "phone",
+                  type: 'input_text',
+                  max_length: 15,
+                  options: []
+                }
+              ]
+            },
+            {
+              attribute: "address_applicant",
+              required: false,
+              label: "Address",
+              description: nil,
+              fields: [
+                {
+                  id: nil,
+                  selector: "address",
+                  type: 'input_text',
+                  max_length: 250,
+                  options: []
+                }
+              ]
+            },
+            {
+              attribute: "avatar",
+              required: false,
+              label: "Avatar",
+              description: nil,
+              fields: [
+                {
+                  id: nil,
+                  selector: 'input[data-ui="avatar"]',
+                  type: "input_file",
+                  max_length: nil,
+                  options: []
+                }
+              ]
+            },
+            {
+              attribute: "work_eligibility",
+              required: true,
+              label: "Work Eligibility",
+              description: "Are you eligible to work in the country that the role is listed?",
+              fields: [
+                {
+                  id: nil,
+                  selector: "work_eligibility", # TODO : check. Previously: 'input[data-ui="avatar"]'
+                  type: 'input_text',
+                  max_length: 250,
+                  options: []
+                }
+              ]
+            },
+            {
+              attribute: "salary_expectations",
+              required: true,
+              label: "Salary expectations",
+              description: "What are your annual salary expectations?",
+              fields: [
+                {
+                  id: nil,
+                  selector: "salary_expectations", # TODO : check. Previously: 'input[data-ui="avatar"]'
+                  type: 'input_text',
+                  max_length: 250,
+                  options: []
+                }
+              ]
+            }
+          ]
+        }
+      ]
 
       PERSONAL_FIELDS = {
         first_name: {
