020514edc5eb3ef92f2d16fa5630c0c575f1ff97
Daniel Sussman <daniel.o.sussman@gmail.com>
2024-08-06 12:33:37 +0100

    removed Workday stuff

--- app/models/concerns/ats/workday/application_fields.rb
+++ app/models/concerns/ats/workday/application_fields.rb
@@ -1,10 +1,8 @@
 module Ats
   module Workday
     module ApplicationFields
-      def get_application_question_set(job, _data)
-        # TODO: Hook up GetWorkdayFields to a formatter and return an application_question_set
+      def get_application_question_set(_job, _data)
         p "Getting Workday application criteria"
-        Importer::GetWorkdayFields.call(job.apply_url) # This isn't hooked up to anything
         []
       end
     end
