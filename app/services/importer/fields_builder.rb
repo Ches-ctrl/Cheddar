a38a55c6fd8fe4990929b5414ab8dcabaf7efd4e
patanj101 <patanj101@gmail.com>
2024-08-07 15:44:31 +0200

    Modify FieldsBuilder documentation comment

--- app/services/importer/fields_builder.rb
+++ app/services/importer/fields_builder.rb
@@ -2,7 +2,7 @@
 
 module Importer
   # This service object builds a structured representation of job application fields based on pre-formatted Greenhouse data.
-  # It relies on the `GreenhouseFieldsFormatter` to prepare the raw data and then constructs a list of field sections.
+  # It relies on FieldsFormatters to prepare the raw data and then constructs a list of field sections.
   # Each section includes details like title, description, build type (API in this case), and individual questions.
   # Question details incorporate attributes, labels, descriptions, required status, and associated field objects.
   # The output of this service is used to the job associated application_question_set.
