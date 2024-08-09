6dc9e2640bda27c8fd9aea5b8d1e9472525d6907
Daniel Sussman <daniel.o.sussman@gmail.com>
2024-08-09 12:01:15 +0100

    rake task is functional except for date fields

--- spec/features/apply_to_job_spec.rb
+++ spec/features/apply_to_job_spec.rb
@@ -2,9 +2,19 @@ require_relative '../support/test_helpers'
 
 include TestHelpers
 
+# This test is designed to be run manually from a rake task.
+# Tests the end-to-end process of job creation -> user application.
+# Testing here means visual confirmation. It will not provide its own metrics.
+# Takes one argument, a posting_url, which is supplied from rake task via ENV.
+# It does the following:
+# - creates a job, user and job_application in the test db
+# - navigates to the application process page in a non-headless browser
+# - fills all form fields with random values
+# - submits the job application to FormFiller
 RSpec.feature 'ApplyToJob', type: :feature, apply_to_job: true do
   before do
     skip 'No URL available to test' if ENV['URL_FOR_TESTING'].nil?
+    @sleep_time = ENV['SLEEP_TIME'] || 10
 
     # switch to visible browser for this test
     @original_driver = Capybara.current_driver
@@ -25,12 +35,8 @@ RSpec.feature 'ApplyToJob', type: :feature, apply_to_job: true do
     login_as(@user)
     visit edit_application_process_job_application_path(@application_process, @job_application)
 
-    all('input[type="text"]').each do |field|
-      field.set("hello")
-    rescue Selenium::WebDriver::Error::ElementNotInteractableError
-      next
-    end
-    sleep 5
+    complete_all_fields
+    submit_form
 
     expect(true).to be_truthy
   end
--- spec/support/test_helpers.rb
+++ spec/support/test_helpers.rb
@@ -1,31 +1,95 @@
+require 'faker'
+
 module TestHelpers
-  def fill_inputs_with_hello
-    all('input').each do |field|
-      field.set("hello")
+  def application_form
+    first('form', visible: true)
+  end
+
+  def attach_resume
+    puts "Attaching resume..."
+    first('input[type="file"]').attach_file(resume_file)
+  end
+
+  def complete_all_fields
+    within application_form do
+      attach_resume
+      complete_input_fields
+      complete_textarea_fields
+      complete_select_fields
+      complete_checkbox_fields
+    end
+    sleep @sleep_time # verify
+  end
+
+  def complete_checkbox_fields
+    puts "Completing checkbox fields..."
+    all('fieldset').each do |fieldset|
+      options = fieldset.all('input[type="checkbox"]')
+      options.sample.click
     end
   end
 
-  def print_hello
-    puts "hello world"
+  def complete_input_fields
+    puts "Completing input fields..."
+    all('input', visible: true)
+      .reject { |field| %w[checkbox email file submit].include?(field['type']) }
+      .each { |field| field.set(Faker::Color.color_name) }
   end
 
-  def initialize_job(url)
-    Builders::AtsBuilder.new.build
-    _, _, job = Url::CreateJobFromUrl.new(url).create_company_then_job
-    job.id
+  def complete_select_fields
+    puts "Completing select fields..."
+    all('select').each do |field|
+      options = field.all('option')[1..] # ignore the null option
+      random_option = options.sample
+      field.select(random_option.text)
+      # sleep 0.2 # avoid obstructing the next element
+    end
   end
 
-  def initialize_user_and_job_application(job_id)
-    puts 'Creating a job and initiating application with admin user...'
+  def complete_textarea_fields
+    puts "Completing textarea fields..."
+    all('textarea').each do |field|
+      field.set(Faker::Quotes::Chiquito.joke)
+    end
+  end
 
+  # quiet mode
+  def do_not_log(&block)
     allow($stdout).to receive(:write) # suppresses terminal clutter
+    result = block.call
+    allow($stdout).to receive(:write).and_call_original # resumes printing to terminal
+    result
+  end
+
+  # seeds ATS to test db, creates job from url and returns job_id
+  def initialize_job(url)
+    puts 'Creating a new job...'
+    do_not_log {
+      Builders::AtsBuilder.new.build
+      _, _, job = Url::CreateJobFromUrl.new(url).create_company_then_job
+      job.id
+    }
+  end
+
+  # With an existing job:
+  # - creates an admin user
+  # - begins an application_process with 1 job_application for user & job
+  def initialize_user_and_job_application(job_id)
+    puts 'Initiating application with admin user...'
 
     user = FactoryBot.create(:user, email: "admin@example.com", admin: true)
     user.saved_jobs.create(job_id:)
     application_process = user.application_processes.create
     job_application = application_process.job_applications.create(job_id:, additional_info: { email: user.user_detail.email })
 
-    allow($stdout).to receive(:write).and_call_original # resumes printing to terminal
     [user, application_process, job_application]
+  end
+
+  def resume_file = 'public/Obretetskiy_cv.pdf'
+
+  def submit_form
+    application_form.find('input[type="submit"]').click
+    find('a', text: 'Submit your applications').click
+    sleep @sleep_time # verify
   end
 end
